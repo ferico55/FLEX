b46a4c6d55758c63bcef9b65160012f2c23f62ba
Tonito <tonito@tokopedia.com>
2014-09-25 16:19:36 +0700

    delete generalthumb

--- Tokopedia/Classses/UI/Home/Hotlist Result/HotlistResultViewCell.m
+++ Tokopedia/Classses/UI/Home/Hotlist Result/HotlistResultViewCell.m
@@ -14,7 +14,7 @@
 #pragma mark - Factory Methods
 + (id)newcell
 {
-    NSArray* a = [[NSBundle mainBundle] loadNibNamed:@"GeneralThumb" owner:nil options:0];
+    NSArray* a = [[NSBundle mainBundle] loadNibNamed:@"HotlistResultViewCell" owner:nil options:0];
     for (id o in a) {
         if ([o isKindOfClass:[self class]]) {
             return o;
