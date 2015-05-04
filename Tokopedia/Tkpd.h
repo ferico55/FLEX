70189703f48a26768a305d6919a0e2eedc31d373
Tonito <tonito@tokopedia.com>
2015-05-04 22:51:13 +0700

    tidy appdelegate

--- Tokopedia/Tkpd.h
+++ Tokopedia/Tkpd.h
@@ -148,4 +148,39 @@ typedef enum {
 
 #define TKPD_ISLOGINNOTIFICATIONNAME @"setlogin"
 
+#ifdef DEBUG
+#define kTKPD_REACHABILITYURL @"http://www.google.com"
+#else
+#define kTKPD_REACHABILITYURL @"http://www.google.com"
+#endif
+#define kTKPD_REACHABILITYDELAY 3.0
+
+#define kTKPD_APSKEY @"aps"
+#define kTKPD_BADGEKEY @"badge"
+
+#define kTKPDWINDOW_TINTLCOLOR [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0]
+
+#define kTKPDNAVIGATION_TABBARACTIVETITLECOLOR [UIColor blackColor]
+#define kTKPDNAVIGATION_TABBARTITLECOLOR [UIColor blackColor]
+
+#define kTKPDNAVIGATION_BACKGROUNDINSET UIEdgeInsetsZero
+#define kTKPDNAVIGATION_TITLEFONT [UIFont fontWithName:@"Lato-Bold" size:16.0f]
+#define kTKPDNAVIGATION_TITLECOLOR [UIColor whiteColor]
+#define kTKPDNAVIGATION_TITLESHADOWCOLOR [UIColor clearColor]
+#define kTKPDNAVIGATION_BUTTONINSET UIEdgeInsetsZero
+#define kTKPDNAVIGATION_BACKBUTTONINSET UIEdgeInsetsMake(0.0f, 35.0f, 0.0f, 0.0f)
+#define kTKPD_SETUSERSTICKYMESSAGEKEY @"stickymessage"
+
+#define kTKPDNAVIGATION_NAVIGATIONBGCOLOR [UIColor colorWithRed:(66/255.0) green:(189/255.0) blue:(65/255.0) alpha:1]
+#define kTKPDNAVIGATION_NAVIGATIONITEMCOLOR [UIColor whiteColor]
+
+#define HelpshiftKey @"a61b53892e353d1828be5154db0ac6c2"
+#define HelpshiftDomain @"tokopedia.helpshift.com"
+#define HelpshiftAppid @"tokopedia_platform_20150407082530564-f41c14c841c644e"
+
+#define GATrackingId @"UA-9801603-10"
+
+#define TokopediaNotificationRedirect @"redirectNotification"
+#define TokopediaNotificationReload @"reloadNotification"
+
 #endif
