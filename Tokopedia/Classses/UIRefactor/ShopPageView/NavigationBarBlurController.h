8c2939a35649952a6fa759731ab4425e983f2462
Harshad <harshad@vxtindia.com>
2015-05-08 16:15:18 +0530

    Added blur effect to the shop container navigation bar

---
+++ Tokopedia/Classses/UIRefactor/ShopPageView/NavigationBarBlurController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationBarBlurController.h
+//  Tokopedia
+//
+//  Created by Harshad Dange on 08/05/2015.
+//  Copyright (c) 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NavigationBarBlurController : NSObject
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/ShopPageView/NavigationBarBlurController.m
@@ -0,0 +1,13 @@
+//
+//  NavigationBarBlurController.m
+//  Tokopedia
+//
+//  Created by Harshad Dange on 08/05/2015.
+//  Copyright (c) 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import "NavigationBarBlurController.h"
+
+@implementation NavigationBarBlurController
+
+@end
