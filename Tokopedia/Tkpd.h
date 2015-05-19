d332c7683c1d96910375bd4cdcba1d4414fd037f
andreTokopedia <andrew.sanjaya@tokopedia.com>
2015-05-19 11:05:34 +0700

    change url ws

--- Tokopedia/Tkpd.h
+++ Tokopedia/Tkpd.h
@@ -11,7 +11,7 @@
 
 //#define kTkpdBaseURLString @"http://www.ef-risky.dvl/ws"
 //#define kTkpdBaseURLString @"http://www.tx-tonito.dvl/ws"
-#define kTkpdBaseURLString @"http://www.tokopedia.com/ws"
+#define kTkpdBaseURLString @"http://beta.tokopedia.com/ws"
 //#define kTkpdBaseURLString @"http://beta.tokopedia.com/ws"
 
 
