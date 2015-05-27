7929b2cfce5eadda89ad7c9bde888fe43baab530
rr <renny_runi@yahoo.co.id>
2015-05-27 15:43:34 +0700

    replace buyer & seller become penjual & pembeli

---
+++ Tokopedia/Classses/Object/UploadImage/UploadImageImage.h
@@ -0,0 +1,13 @@
+//
+//  UploadImageImage.h
+//  Tokopedia
+//
+//  Created by IT Tkpd on 5/27/15.
+//  Copyright (c) 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UploadImageImage : NSObject
+
+@end
