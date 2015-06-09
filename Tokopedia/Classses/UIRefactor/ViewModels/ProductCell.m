b2f96f3249983898404a199b4def75c19c126bd2
Tonito <tonito@tokopedia.com>
2015-06-09 18:40:10 +0700

    set product content mode image

--- Tokopedia/Classses/UIRefactor/ViewModels/ProductCell.m
+++ Tokopedia/Classses/UIRefactor/ViewModels/ProductCell.m
@@ -20,19 +20,19 @@
     [self.productName setText:viewModel.productName];
     [self.productPrice setText:viewModel.productPrice];
     [self.productShop setText:viewModel.productShop];
+    self.goldShopBadge.hidden = viewModel.isGoldShopProduct ? NO : YES;
     
     NSURLRequest* request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:viewModel.productThumbUrl] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:kTKPDREQUEST_TIMEOUTINTERVAL];
     
-    [self.productImage setImageWithURLRequest:request placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-retain-cycles"
-        [self.productImage setImage:image];
+    [self.productImage setContentMode:UIViewContentModeCenter];
+    [self.productImage setImageWithURLRequest:request placeholderImage:[UIImage imageNamed:@"icon_toped_loading_grey-02.png"] success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Warc-retain-cycles"
         [self.productImage setContentMode:UIViewContentModeScaleAspectFill];
+        [self.productImage setImage:image];
     } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
         [self.productImage setImage:[UIImage imageNamed:@""]];
     }];
-    
-    //TODO::gold badge here
     
 }
 
