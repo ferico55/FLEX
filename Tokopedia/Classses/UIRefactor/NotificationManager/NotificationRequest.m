c3cc9f584bb2b021c5be63d767be91ca9959bbf4
Tonito <tonito@tokopedia.com>
2015-06-15 10:53:15 +0700

    Add requestTimeout to 16s

--- Tokopedia/Classses/UIRefactor/NotificationManager/NotificationRequest.m
+++ Tokopedia/Classses/UIRefactor/NotificationManager/NotificationRequest.m
@@ -179,8 +179,8 @@
     }];
     
     [_operationQueue addOperation:_request];
-
-    _requestTimer = [NSTimer scheduledTimerWithTimeInterval:kTKPDREQUEST_TIMEOUTINTERVAL target:self selector:@selector(requestTimeout) userInfo:nil repeats:NO];
+    
+    _requestTimer = [NSTimer scheduledTimerWithTimeInterval:16.0 target:self selector:@selector(requestTimeout) userInfo:nil repeats:NO];
     [[NSRunLoop currentRunLoop] addTimer:_requestTimer forMode:NSRunLoopCommonModes];
 }
 
