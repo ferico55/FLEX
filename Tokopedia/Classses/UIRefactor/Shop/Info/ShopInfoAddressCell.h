9cfc59dbbb3347b26d4bf6faeaf2fae273aa3c60
rennyrun <renny.runiawati@tokopedia.com>
2015-06-17 16:43:13 +0700

    replace scrollview with tableview

---
+++ Tokopedia/Classses/UIRefactor/Shop/Info/ShopInfoAddressCell.h
@@ -0,0 +1,15 @@
+//
+//  ShopInfoAddressCell.h
+//  Tokopedia
+//
+//  Created by Renny Runiawati on 6/17/15.
+//  Copyright (c) 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopInfoAddressCell : UITableViewCell
+
++ (id)newcell;
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/Shop/Info/ShopInfoAddressCell.m
@@ -0,0 +1,34 @@
+//
+//  ShopInfoAddressCell.m
+//  Tokopedia
+//
+//  Created by Renny Runiawati on 6/17/15.
+//  Copyright (c) 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import "ShopInfoAddressCell.h"
+
+@implementation ShopInfoAddressCell
+
++ (id)newcell
+{
+    NSArray* a = [[NSBundle mainBundle] loadNibNamed:@"ShopInfoAddressCell" owner:nil options:0];
+    for (id o in a) {
+        if ([o isKindOfClass:[self class]]) {
+            return o;
+        }
+    }
+    return nil;
+}
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
