bc598eedb425ca7b093e021fc608e2cdf9444082
Tonito <tonito@tokopedia.com>
2015-06-19 11:28:02 +0700

    Add git commit message to ReleaseNotes

--- iOS_Build.sh
+++ iOS_Build.sh
@@ -15,3 +15,9 @@ rm -rf "./JenkinsIPAExport/*"
 mkdir "./JenkinsIPAExport"
 
 xcodebuild -exportArchive -exportFormat IPA -exportProvisioningProfile Ad\ Hoc\ Distribution -archivePath ./JenkinsArchive/Tokopedia.xcarchive -exportPath ./JenkinsIPAExport/Tokopedia.ipa
+
+
+#Write Release Notes to Crashlytics
+echo "$(git log --pretty="%h - %s" --since=5.days --no-merges)" > ReleaseNotes.txt
+
+./Crashlytics.framework/submit "d02324f74e70ffcd2dc00ab722d32baecf1884b4" "86d3efd816cabb133f016f397c41c066a22980ee54ed0805f7f73897d4cfc61e" -emails tonito@tokopedia.com -notesPath  ReleaseNotes.txt -groupAliases ﻿qa -ipaPath ./JenkinsIPAExport/Tokopedia.ipa
