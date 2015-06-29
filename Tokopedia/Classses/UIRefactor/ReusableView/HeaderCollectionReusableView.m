385ba178ea58a720ba763c152011bdacb42a40d6
Tonito <tonito@tokopedia.com>
2015-06-29 16:10:13 +0700

    #ignore | Add Files autolayout

---
+++ Tokopedia/Classses/UIRefactor/ReusableView/HeaderCollectionReusableView.m
@@ -0,0 +1,17 @@
+//
+//  HeaderCollectionReusableView.m
+//  Tokopedia
+//
+//  Created by Tokopedia on 6/12/15.
+//  Copyright (c) 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import "HeaderCollectionReusableView.h"
+
+@implementation HeaderCollectionReusableView
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
