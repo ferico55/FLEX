515b628bba96c5646f64c7cd1bf49cbd1e115144
rennyrun <renny.runiawati@tokopedia.com>
2015-07-30 16:33:38 +0700

    #ignore | create object for indomaret and klikbca additional data

---
+++ Tokopedia/Classses/Object/Transaction/TransactionSummary/IndomaretData.h
@@ -0,0 +1,13 @@
+//
+//  IndomaretData.h
+//  Tokopedia
+//
+//  Created by Renny Runiawati on 7/30/15.
+//  Copyright (c) 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IndomaretData : NSObject
+
+@end
