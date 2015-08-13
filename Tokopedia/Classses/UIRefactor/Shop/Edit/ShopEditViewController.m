7a7f99cd12aafdac63c5fe66ff895a79c66b9516
feizalbadri <feizal.asmoro@tokopedia.com>
2015-08-13 14:38:06 +0700

    hotfix

--- Tokopedia/Classses/UIRefactor/Shop/Edit/ShopEditViewController.m
+++ Tokopedia/Classses/UIRefactor/Shop/Edit/ShopEditViewController.m
@@ -556,19 +556,20 @@
             {
                 //save
                 NSMutableArray *message = [NSMutableArray new];
-                NSString *shopdesc = [_datainput objectForKey:kTKPDSHOPEDIT_APISHORTDESCKEY]?:_shop.info.shop_description;
-                NSString *tagline = [_datainput objectForKey:kTKPDSHOPEDIT_APITAGLINEKEY]?:_shop.info.shop_description;
+                NSString *shopdesc = [_datainput objectForKey:kTKPDSHOPEDIT_APISHORTDESCKEY];
+                NSString *tagline = [_datainput objectForKey:kTKPDSHOPEDIT_APITAGLINEKEY];
                 if (shopdesc.length == 0) {
                     [message addObject:@"Deskripsi harus diisi."];
-                } else if (tagline.length == 0) {
+                }
+                if (tagline.length == 0) {
                     [message addObject:@"Slogan harus diisi."];
-                } else {
-                    [self configureRestKit];
-                    [self requestaction:_datainput];
                 }
                 if (message.count>0) {
                     StickyAlertView *alert = [[StickyAlertView alloc] initWithErrorMessages:message delegate:self];
                     [alert show];
+                } else {
+                    [self configureRestKit];
+                    [self requestaction:_datainput];
                 }
                 break;
             }
