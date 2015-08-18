a1d9d7027e03bd2afa5a5f2a9063fbe8a8732f94
Tonito <tonito@tokopedia.com>
2015-08-18 11:27:16 +0700

    #UI | minutes only 1 char

--- Tokopedia/Classses/UIRefactor/Talk/ProductTalkDetailViewController.m
+++ Tokopedia/Classses/UIRefactor/Talk/ProductTalkDetailViewController.m
@@ -987,7 +987,7 @@
                     
                     NSDate *today = [NSDate date];
                     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-                    [dateFormat setDateFormat:@"dd MMMM yyyy, HH:m"];
+                    [dateFormat setDateFormat:@"dd MMMM yyyy, HH:mm"];
                     NSString *dateString = [dateFormat stringFromDate:today];
                     
                     commentlist.comment_create_time = dateString;
