cccaa4e00c3fe659501f674ca4a8b1a559991c66
feizalbadri <feizal.asmoro@tokopedia.com>
2015-08-28 16:01:18 +0700

    google plus

--- Tokopedia/Classses/UIRefactor/Activation/CreatePassword/CreatePasswordViewController.m
+++ Tokopedia/Classses/UIRefactor/Activation/CreatePassword/CreatePasswordViewController.m
@@ -16,6 +16,7 @@
 #import <FacebookSDK/FacebookSDK.h>
 #import "AlertDatePickerView.h"
 #import "TKPDAlert.h"
+#import "WebViewController.h"
 
 @interface CreatePasswordViewController ()
 <
@@ -55,7 +56,6 @@
 @property (weak, nonatomic) IBOutlet TextField *confirmPasswordTextfield;
 
 @property (weak, nonatomic) IBOutlet UIButton *agreementButton;
-@property (weak, nonatomic) IBOutlet UILabel *agreementLabel;
 
 @property (weak, nonatomic) IBOutlet UIButton *signupButton;
 
@@ -76,17 +76,14 @@
                                                               action:@selector(tap:)];
     self.navigationItem.leftBarButtonItem = button;
     
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@""
+                                                                   style:UIBarButtonItemStyleBordered
+                                                                  target:nil
+                                                                  action:nil];
+    self.navigationItem.backBarButtonItem = backButton;
+    
     NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
     style.lineSpacing = 4.0;
-    
-    NSDictionary *agreementAttributes = @{
-        NSFontAttributeName            : [UIFont fontWithName:@"GothamBook" size:12],
-        NSParagraphStyleAttributeName  : style,
-        NSForegroundColorAttributeName : [UIColor lightGrayColor],
-    };
-
-    _agreementLabel.attributedText = [[NSAttributedString alloc] initWithString:_agreementLabel.text
-                                                                     attributes:agreementAttributes];
 
     NSMutableParagraphStyle *descriptionStyle = [[NSMutableParagraphStyle alloc] init];
     descriptionStyle.lineSpacing = 4.0;
@@ -109,13 +106,42 @@
     _operationQueue = [NSOperationQueue new];
     _requestCount = 0;
     
-    _fullNameTextField.text = [_facebookUser objectForKey:@"name"];
+    NSString *name;
+    if (_facebookUser) {
+        name = [_facebookUser objectForKey:@"name"];
+    } else if (_googleUser) {
+        name = [_googleUser.name.givenName stringByAppendingFormat:@" %@", _googleUser.name.familyName];
+    }
+    _fullNameTextField.text = name;
     
-    _emailTextField.text = [_facebookUser objectForKey:@"email"];
+    
+    NSString *email;
+    if (_facebookUser) {
+        email = [_facebookUser objectForKey:@"email"];
+    } else if (_googleUser) {
+        GPPSignIn *signIn = [GPPSignIn sharedInstance];
+        email = signIn.userEmail;
+    }
+    _emailTextField.text = email;
     _emailTextField.enabled = NO;
     _emailTextField.layer.opacity = 0.7;
     
-    _dateOfBirthTextField.text = [_facebookUser objectForKey:@"birthday"];
+    NSString *birthday = @"";
+    if (_facebookUser) {
+        birthday = [_facebookUser objectForKey:@"birthday"];
+    } else if (_googleUser) {
+        if (_googleUser.birthday) {
+            NSArray *birthdayComponents = [_googleUser.birthday componentsSeparatedByString:@"-"];
+            NSString *year = [birthdayComponents objectAtIndex:0];
+            if (![year isEqualToString:@"0000"]) {
+                NSString *day = [birthdayComponents objectAtIndex:2];
+                NSString *month = [birthdayComponents objectAtIndex:1];
+                birthday = [NSString stringWithFormat:@"%@/%@/%@", day, month, year];
+            }
+        }
+    }
+    
+    _dateOfBirthTextField.text = birthday;
     _dateOfBirthTextField.delegate = self;
     
     _activityIndicatorView.hidden = YES;
@@ -130,12 +156,34 @@
              object:nil];
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    
-    [_scrollView addSubview:_contentView];
+    [self updateFormViewAppearance];
+}
 
+- (void)updateFormViewAppearance {
+    CGRect frame = _contentView.frame;
+    CGFloat contentViewWidth = [[UIScreen mainScreen] bounds].size.width;
+    CGFloat contentViewMarginLeft = 0;
+    CGFloat contentViewMarginTop = 0;
+    if (IS_IPHONE_6) {
+        contentViewWidth = 335;
+        contentViewMarginLeft = 20;
+        contentViewMarginTop = 20;
+    } else if (IS_IPHONE_6P) {
+        contentViewWidth = 354;
+        contentViewMarginLeft = 30;
+        contentViewMarginTop = 40;
+    } else if (IS_IPAD) {
+        contentViewWidth = 500;
+        contentViewMarginLeft = 134;
+        contentViewMarginTop = 134;
+    }
+    frame.size.width = contentViewWidth;
+    frame.origin.x = contentViewMarginLeft;
+    frame.origin.y = contentViewMarginTop;
+    _contentView.frame = frame;
+    
     CGFloat contentSizeHeight;
     if (self.view.frame.size.height > _contentView.frame.size.height) {
         contentSizeHeight = self.view.frame.size.height+1;
@@ -143,6 +191,8 @@
         contentSizeHeight = _contentView.frame.size.height;
     }
     _scrollView.contentSize = CGSizeMake(self.view.frame.size.width, contentSizeHeight);
+
+    [_scrollView addSubview:_contentView];
 }
 
 - (void)updateViewConstraints {
@@ -236,6 +286,22 @@
     }
 }
 
+- (IBAction)tapTerms:(id)sender {
+    WebViewController *webViewController = [WebViewController new];
+    webViewController.strTitle = @"Syarat & Ketentuan";
+    webViewController.strURL = @"https://m.tokopedia.com/terms.pl";
+    [self.navigationController pushViewController:webViewController animated:YES];
+}
+
+- (IBAction)tapPrivacy:(id)sender {
+    WebViewController *webViewController = [WebViewController new];
+    webViewController.strTitle = @"Kebijakan Privasi";
+    webViewController.strURL = @"https://m.tokopedia.com/privacy.pl";
+    [self.navigationController pushViewController:webViewController animated:YES];
+}
+
+#pragma mark - Restkit
+
 - (void)configureRestKit
 {
     // initialize RestKit
@@ -295,9 +361,11 @@
     NSArray *dataComponents = [_dateOfBirthTextField.text componentsSeparatedByString:@"/"];
     
     NSString *gender = @"";
-    if ([[_facebookUser objectForKey:@"gender"] isEqualToString:@"male"]) {
+    if ([[_facebookUser objectForKey:@"gender"] isEqualToString:@"male"] ||
+        [_googleUser.gender isEqualToString:@"male"]) {
         gender = @"1";
-    } else if ([[_facebookUser objectForKey:@"gender"] isEqualToString:@"female"]) {
+    } else if ([[_facebookUser objectForKey:@"gender"] isEqualToString:@"female"] ||
+               [_googleUser.gender isEqualToString:@"female"]) {
         gender = @"2";
     }
     
