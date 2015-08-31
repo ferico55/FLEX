0f915a22db1e61ab9f65b46294a62b5d717c8d91
feizalbadri <feizal.asmoro@tokopedia.com>
2015-08-31 15:40:59 +0700

    on progress

--- Tokopedia/Classses/UIRefactor/ContactUs/ContactUsViewController.m
+++ Tokopedia/Classses/UIRefactor/ContactUs/ContactUsViewController.m
@@ -13,6 +13,7 @@
 #import "string_contact_us.h"
 #import "ContactUsFormViewController.h"
 #import "ContactUsTypeCell.h"
+#import "GeneralTableViewController.h"
 
 @interface ContactUsViewController ()
 <
@@ -21,12 +22,17 @@
     UICollectionViewDataSource,
     UICollectionViewDelegate,
     UICollectionViewDelegateFlowLayout,
-    TokopediaNetworkManagerDelegate
+    TokopediaNetworkManagerDelegate,
+    GeneralTableViewControllerDelegate
 >
 {
     TokopediaNetworkManager *_networkManager;
     RKObjectManager *_objectManager;
     NSArray *_categories;
+    NSIndexPath *_selectedCollectionIndexPath;
+    TicketCategory *_selectedType;
+    TicketCategory *_selectedProblem;
+    TicketCategory *_selectedDetailProblem;
 }
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -40,6 +46,7 @@
 @property (strong, nonatomic) IBOutlet UIView *footerView;
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *flowLayout;
+@property (weak, nonatomic) IBOutlet UIWebView *descriptionWebView;
 
 @end
 
@@ -53,8 +60,6 @@
     _networkManager = [TokopediaNetworkManager new];
     _networkManager.delegate = self;
     [_networkManager doRequest];
-
-    self.tableView.tableFooterView = _footerView;
     
     self.hidesBottomBarWhenPushed = YES;
     
@@ -79,16 +84,27 @@
 #pragma mark - Table view delegate
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 3;
+    NSInteger sections;
+    if (_categories.count > 0) {
+        sections = 3;
+    } else {
+        sections = 0;
+    }
+    return sections;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSInteger rows;
+    NSInteger rows = 0;
     if (section == 0) {
         rows = 1;
     } else if (section == 1) {
-        rows = 2;
-    } else if (section == 2) {
+        if (_selectedType) {
+            rows = 1;
+        }
+        if (_selectedProblem) {
+            rows = 2;
+        }
+    } else if (section == 2 && _selectedDetailProblem) {
         rows = 1;
     }
     return rows;
@@ -101,8 +117,10 @@
     } else if (indexPath.section == 1) {
         if (indexPath.row == 0) {
             cell = _problemCell;
+            if (_selectedProblem) cell.detailTextLabel.text = _selectedProblem.ticket_category_name?:@"Pilih";
         } else if (indexPath.row == 1) {
             cell = _problemDetailCell;
+            if (_selectedDetailProblem) cell.detailTextLabel.text = _selectedDetailProblem.ticket_category_name?:@"Pilih";
         }
     } else if (indexPath.section == 2) {
         cell = _problemSolutionCell;
@@ -134,9 +152,41 @@
     } else if (indexPath.section == 1) {
         height = 44;
     } else if (indexPath.section == 2) {
-        height = 44;
+        height = 200;
     }
     return height;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 1) {
+        NSString *title;
+        NSMutableArray *choices = [NSMutableArray new];
+        TicketCategory *selectedCategory;
+        if (indexPath.row == 0) {
+            title = @"Pilih Masalah";
+            selectedCategory = _selectedProblem;
+            for (TicketCategory *category in _selectedType.ticket_category_child) {
+                [choices addObject:category.ticket_category_name];
+            }
+        } else if (indexPath.row == 1) {
+            title = @"Pilih Detail Masalah";
+            selectedCategory = _selectedDetailProblem;
+            for (TicketCategory *category in _selectedProblem.ticket_category_child) {
+                [choices addObject:category.ticket_category_name];
+            }
+        }
+        
+        GeneralTableViewController *controller = [GeneralTableViewController new];
+        controller.title = title;
+        controller.delegate = self;
+        controller.senderIndexPath = indexPath;
+        controller.objects = choices;
+        controller.selectedObject = selectedCategory.ticket_category_name;
+        
+        [self.navigationController pushViewController:controller animated:YES];
+    }
 }
 
 #pragma mark - Collection view data source
@@ -151,6 +201,21 @@
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     ContactUsTypeCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ContactUsTypeCell" forIndexPath:indexPath];
+    cell.imageView.layer.cornerRadius = cell.imageView.frame.size.width / 2;
+    cell.imageView.layer.borderWidth = 0;
+    if (_selectedCollectionIndexPath) {
+        cell.alpha = 0.5;
+        if (indexPath.row == _selectedCollectionIndexPath.row) {
+            cell.imageView.layer.borderWidth = 5;
+            cell.imageView.layer.borderColor = [UIColor colorWithRed:18/255.0
+                                                               green:199/255.0
+                                                                blue:0/255.0
+                                                               alpha:1].CGColor;
+            cell.alpha = 1;
+        }
+    } else {
+        cell.alpha = 1;
+    }
     return cell;
 }
 
@@ -161,11 +226,17 @@
 #pragma mark - Collection view delegate
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    ContactUsTypeCell *cell = (ContactUsTypeCell *)[collectionView cellForItemAtIndexPath:indexPath];
-    cell.imageView.layer.cornerRadius = cell.imageView.frame.size.width / 2;
-    cell.imageView.layer.borderWidth = 5;
-    cell.imageView.layer.borderColor = [UIColor colorWithRed:18/255.0 green:199/255.0 blue:0/255.0 alpha:1].CGColor;
-    cell.layer.opacity = 1;
+    _selectedCollectionIndexPath = indexPath;
+    if (indexPath.section == 1) {
+        if (indexPath.row == 0) {
+            
+        } else if (indexPath.row == 1) {
+            
+        }
+    }
+    _selectedType = [_categories objectAtIndex:indexPath.row];
+    [collectionView reloadData];
+    [self.tableView reloadData];
 }
 
 #pragma mark - Network manager
@@ -269,6 +340,7 @@
 - (void)actionAfterRequest:(RKMappingResult *)mappingResult withOperation:(RKObjectRequestOperation *)operation withTag:(int)tag {
     ContactUsResponse *response = [mappingResult.dictionary objectForKey:@""];
     _categories = response.result.list;
+    [self.tableView reloadData];
 }
 
 - (void)actionFailAfterRequest:(id)errorResult withTag:(int)tag {
@@ -287,6 +359,86 @@
             [self.navigationController pushViewController:controller animated:YES];
         }
     }
+}
+
+#pragma mark - General table delegate
+
+- (void)didSelectObject:(id)object senderIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == 1) {
+        if (indexPath.row == 0) {
+            for (TicketCategory *category in _selectedType.ticket_category_child) {
+                if ([category.ticket_category_name isEqualToString:object]) {
+                    _selectedProblem = category;
+                }
+            }
+        } else if (indexPath.row == 1) {
+            for (TicketCategory *category in _selectedProblem.ticket_category_child) {
+                if ([category.ticket_category_name isEqualToString:object]) {
+                    _selectedDetailProblem = category;
+                    NSString *string = category.ticket_category_description;
+                    UIFont *font = [UIFont fontWithName:@"Helvetica" size:12];
+                    UIColor *color = [UIColor blackColor];
+                    NSString *description = [self htmlFromBodyString:string
+                                                            textFont:font
+                                                           textColor:color];
+                    [self.descriptionWebView loadHTMLString:description baseURL:nil];
+                    self.descriptionWebView.delegate = self;
+                    self.descriptionWebView.scrollView.scrollEnabled = NO;
+                    self.descriptionWebView.scrollView.bounces = NO;
+                }
+            }
+        }
+    }
+    [self.tableView reloadData];
+}
+
+#pragma mark - Web view methods
+
+-(CGSize)sizeOfText:(NSString *)textToMesure widthOfTextView:(CGFloat)width withFont:(UIFont*)font
+{
+    CGSize ts = [textToMesure sizeWithFont:font
+                         constrainedToSize:CGSizeMake(width-20.0, FLT_MAX)
+                             lineBreakMode:NSLineBreakByWordWrapping];
+    return ts;
+}
+
+- (NSString *)htmlFromBodyString:(NSString *)htmlBodyString
+                        textFont:(UIFont *)font
+                       textColor:(UIColor *)textColor
+{
+    int numComponents = CGColorGetNumberOfComponents([textColor CGColor]);
+    
+    NSAssert(numComponents == 4 || numComponents == 2, @"Unsupported color format");
+    
+    // E.g. FF00A5
+    NSString *colorHexString = nil;
+    
+    const CGFloat *components = CGColorGetComponents([textColor CGColor]);
+    
+    if (numComponents == 4)
+    {
+        unsigned int red = components[0] * 255;
+        unsigned int green = components[1] * 255;
+        unsigned int blue = components[2] * 255;
+        colorHexString = [NSString stringWithFormat:@"%02X%02X%02X", red, green, blue];
+    }
+    else
+    {
+        unsigned int white = components[0] * 255;
+        colorHexString = [NSString stringWithFormat:@"%02X%02X%02X", white, white, white];
+    }
+    
+    NSString *HTML = [NSString stringWithFormat:@"<html>\n"
+                      "<head>\n"
+                      "<style type=\"text/css\">\n"
+                      "body {font-family: \"%@\"; font-size: %@; color:#%@;}\n"
+                      "</style>\n"
+                      "</head>\n"
+                      "<body>%@</body>\n"
+                      "</html>",
+                      font.familyName, @(font.pointSize), colorHexString, htmlBodyString];
+    
+    return HTML;
 }
 
 @end
