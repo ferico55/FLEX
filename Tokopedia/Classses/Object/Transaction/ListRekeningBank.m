21841b2eaf709d84c5dfe5c68e8bcbdfbbeb2909
rennyrun <renny.runiawati@tokopedia.com>
2015-09-29 15:58:52 +0700

    add array default

--- Tokopedia/Classses/Object/Transaction/ListRekeningBank.m
+++ Tokopedia/Classses/Object/Transaction/ListRekeningBank.m
@@ -9,6 +9,7 @@
 #define CacheFileNameListSystemBank @"list_bank"
 
 #import "ListRekeningBank.h"
+#import "TransactionBuy.h"
 
 @implementation ListRekeningBank
 
@@ -21,37 +22,80 @@
     return path;
 }
 
--(RKObjectManager*)objectManager
+-(NSArray*)defaultListBank
 {
-    RKObjectManager *objectManagerActionBuy = [RKObjectManager sharedClient];
-    
-    // setup object mappings
-    RKObjectMapping *statusMapping = [RKObjectMapping mappingForClass:[TransactionBuy class]];
-    [statusMapping addAttributeMappingsFromDictionary:@{kTKPD_APISTATUSMESSAGEKEY:kTKPD_APISTATUSMESSAGEKEY,
-                                                        kTKPD_APIERRORMESSAGEKEY:kTKPD_APIERRORMESSAGEKEY,
-                                                        kTKPD_APISTATUSKEY:kTKPD_APISTATUSKEY,
-                                                        kTKPD_APISERVERPROCESSTIMEKEY:kTKPD_APISERVERPROCESSTIMEKEY,
-                                                        }];
-    
-    RKObjectMapping *resultMapping = [RKObjectMapping mappingForClass:[TransactionBuyResult class]];
-    [resultMapping addAttributeMappingsFromArray:@[kTKPD_APIISSUCCESSKEY]];
-    
-    RKObjectMapping *systemBankMapping  = [[self mapping] systemBankMapping];
-    
-    [statusMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:kTKPD_APIRESULTKEY toKeyPath:kTKPD_APIRESULTKEY withMapping:resultMapping]];
-    
-    [resultMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:API_TRANSACTION_SUMMARY_KEY toKeyPath:API_TRANSACTION_SUMMARY_KEY withMapping:transactionMapping]];
-    
-    RKRelationshipMapping *systemBankRel = [RKRelationshipMapping relationshipMappingFromKeyPath:API_SYSTEM_BANK_KEY toKeyPath:API_SYSTEM_BANK_KEY withMapping:systemBankMapping];
-    [resultMapping addPropertyMapping:systemBankRel];
-    
-    // register mappings with the provider using a response descriptor
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:statusMapping method:RKRequestMethodPOST pathPattern:API_TRANSACTION_PATH keyPath:@"" statusCodes:kTkpdIndexSetStatusCodeOK];
-    
-    [objectManagerActionBuy addResponseDescriptor:responseDescriptor];
-    
-    return objectManagerActionBuy;
+ NSArray *system_bank = [
+                        {
+                            sb_active: 1,
+                            sb_account_no: "372 177 3939",
+                            sb_bank_name: "BCA",
+                            sb_bank_cabang: "Kedoya Permai",
+                            sb_picture: "https://cdn-alpha.tokopedia.com/img/icon-bca.png",
+                            sb_info: "Verifikasi 1x24 jam",
+                            sb_account_name: "PT. Tokopedia"
+                        },
+                        {
+                            sb_account_name: "Tokopedia",
+                            sb_info: "Verifikasi 2x24 jam",
+                            sb_picture: "https://cdn-alpha.tokopedia.com/img/icon-mandiri.png",
+                            sb_bank_cabang: "Permata Hijau",
+                            sb_bank_name: "MANDIRI",
+                            sb_active: 1,
+                            sb_account_no: "102 000 5263873"
+                        },
+                        {
+                            sb_account_name: "PT. Tokopedia",
+                            sb_info: "Verifikasi 1x24 jam",
+                            sb_picture: "https://cdn-alpha.tokopedia.com/img/icon-bni.png",
+                            sb_bank_cabang: "Siloam - Kebon Jeruk",
+                            sb_bank_name: "BNI",
+                            sb_active: 1,
+                            sb_account_no: "800 600 6009"
+                        },
+                        {
+                            sb_bank_cabang: "Kebon Jeruk",
+                            sb_info: "Verifikasi 1x24 jam",
+                            sb_picture: "https://cdn-alpha.tokopedia.com/img/icon-bri.png",
+                            sb_active: 1,
+                            sb_account_no: "037 701 000435301",
+                            sb_bank_name: "BRI",
+                            sb_account_name: "PT. Tokopedia"
+                        }
+        ]
+    }
 }
+
+//-(RKObjectManager*)objectManager
+//{
+//    RKObjectManager *objectManagerActionBuy = [RKObjectManager sharedClient];
+//    
+//    // setup object mappings
+//    RKObjectMapping *statusMapping = [RKObjectMapping mappingForClass:[TransactionBuy class]];
+//    [statusMapping addAttributeMappingsFromDictionary:@{kTKPD_APISTATUSMESSAGEKEY:kTKPD_APISTATUSMESSAGEKEY,
+//                                                        kTKPD_APIERRORMESSAGEKEY:kTKPD_APIERRORMESSAGEKEY,
+//                                                        kTKPD_APISTATUSKEY:kTKPD_APISTATUSKEY,
+//                                                        kTKPD_APISERVERPROCESSTIMEKEY:kTKPD_APISERVERPROCESSTIMEKEY,
+//                                                        }];
+//    
+//    RKObjectMapping *resultMapping = [RKObjectMapping mappingForClass:[TransactionBuyResult class]];
+//    [resultMapping addAttributeMappingsFromArray:@[kTKPD_APIISSUCCESSKEY]];
+//    
+////    RKObjectMapping *systemBankMapping  = [[self mapping] systemBankMapping];
+////    
+////    [statusMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:kTKPD_APIRESULTKEY toKeyPath:kTKPD_APIRESULTKEY withMapping:resultMapping]];
+////    
+////    [resultMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:API_TRANSACTION_SUMMARY_KEY toKeyPath:API_TRANSACTION_SUMMARY_KEY withMapping:transactionMapping]];
+////    
+////    RKRelationshipMapping *systemBankRel = [RKRelationshipMapping relationshipMappingFromKeyPath:API_SYSTEM_BANK_KEY toKeyPath:API_SYSTEM_BANK_KEY withMapping:systemBankMapping];
+////    [resultMapping addPropertyMapping:systemBankRel];
+////    
+////    // register mappings with the provider using a response descriptor
+////    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:statusMapping method:RKRequestMethodPOST pathPattern:API_TRANSACTION_PATH keyPath:@"" statusCodes:kTkpdIndexSetStatusCodeOK];
+//    
+//    [objectManagerActionBuy addResponseDescriptor:responseDescriptor];
+//    
+//    return objectManagerActionBuy;
+//}
 
 - (id)getFromCache {
     NSError* error;
@@ -66,10 +110,10 @@
         }
         
         NSMutableDictionary *mappingsDictionary = [[NSMutableDictionary alloc] init];
-        _objectmanager = [self getObjectManager:0];
-        for (RKResponseDescriptor *descriptor in _objectmanager.responseDescriptors) {
-            [mappingsDictionary setObject:descriptor.mapping forKey:descriptor.keyPath];
-        }
+        //_objectmanager = [self getObjectManager:0];
+//        for (RKResponseDescriptor *descriptor in _objectmanager.responseDescriptors) {
+//            [mappingsDictionary setObject:descriptor.mapping forKey:descriptor.keyPath];
+//        }
         
         RKMapperOperation *mapper = [[RKMapperOperation alloc] initWithRepresentation:parsedData
                                                                    mappingsDictionary:mappingsDictionary];
