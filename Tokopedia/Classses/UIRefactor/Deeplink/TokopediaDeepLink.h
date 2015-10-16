3f5621e66e718f87cc6f9c1fe7e96b1c24b0d067
feizalbadri <feizal.asmoro@tokopedia.com>
2015-10-16 10:48:26 +0700

    pod

---
+++ Tokopedia/Classses/UIRefactor/Deeplink/TokopediaDeepLink.h
@@ -0,0 +1,13 @@
+//
+//  TokopediaDeepLink.h
+//  Tokopedia
+//
+//  Created by Tokopedia on 10/16/15.
+//  Copyright © 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TokopediaDeepLink : NSObject
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/Deeplink/TokopediaDeepLink.m
@@ -0,0 +1,13 @@
+//
+//  TokopediaDeepLink.m
+//  Tokopedia
+//
+//  Created by Tokopedia on 10/16/15.
+//  Copyright © 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import "TokopediaDeepLink.h"
+
+@implementation TokopediaDeepLink
+
+@end
