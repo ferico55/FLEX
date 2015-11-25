1f5173b899b435b02ffadd7cca54135c3a293ad3
Johanes Effendi <johaneseffendi@Tokopedias-Mac-mini-2.local>
2015-11-25 11:28:02 +0700

    add phone verif page

---
+++ Tokopedia/Classses/UIRefactor/Activation/PhoneVerification/PhoneVerificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhoneVerificationViewController.h
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 11/25/15.
+//  Copyright © 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhoneVerificationViewController : UIViewController
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/Activation/PhoneVerification/PhoneVerificationViewController.m
@@ -0,0 +1,37 @@
+//
+//  PhoneVerificationViewController.m
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 11/25/15.
+//  Copyright © 2015 TOKOPEDIA. All rights reserved.
+//
+
+#import "PhoneVerificationViewController.h"
+
+@interface PhoneVerificationViewController ()
+
+@end
+
+@implementation PhoneVerificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
