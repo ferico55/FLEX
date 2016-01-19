b430abffbc72a22a6f23ffedcb904e75a82d615f
Kenneth Vincent <kennethvincent@Kenneths-Mac-mini.local>
2016-01-19 15:57:27 +0700

    create add caption on product file

---
+++ Tokopedia/Classses/UIRefactor/Product/ProductAddCaptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductAddCaptionViewController.h
+//  Tokopedia
+//
+//  Created by Kenneth Vincent on 1/19/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductAddCaptionViewController : UIViewController
+
+@end
