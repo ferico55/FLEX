e749de2ee2b58e4985f6f8d6920dee8ae714fc9a
Tonito <tonito@tokopedia.com>
2016-01-21 12:07:16 +0700

    view will always different

--- Tokopedia/Classses/UIRefactor/Category/CarouselDataSource.m
+++ Tokopedia/Classses/UIRefactor/Category/CarouselDataSource.m
@@ -32,17 +32,14 @@ NSInteger const bannerIpadWidth = 350;
 }
 
 - (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view {
-    if(view == nil) {
-        CGFloat bannerWidth;
-        if(IS_IPAD) {
-            bannerWidth = bannerIpadWidth;
-        } else {
-            bannerWidth = [UIScreen mainScreen].bounds.size.width;
-        }
-        view = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, bannerWidth, bannerHeight)];
-
+    CGFloat bannerWidth;
+    if(IS_IPAD) {
+        bannerWidth = bannerIpadWidth;
+    } else {
+        bannerWidth = [UIScreen mainScreen].bounds.size.width;
     }
-    
+
+    view = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, bannerWidth, bannerHeight)];
     BannerList *banner = _banners[index];
     [(UIImageView *)view setImageWithURL:[NSURL URLWithString:banner.img_uri] placeholderImage:[UIImage imageNamed:@"icon_toped_loading_grey-02.png"]];
     
