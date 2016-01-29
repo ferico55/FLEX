321fb5925999c8464fae8c5f5aaedb06daced496
Kenneth Vincent <kennethvincent@Kenneths-Mac-mini.local>
2016-01-29 12:00:33 +0700

    add give review detail page and its constraints

---
+++ Tokopedia/Classses/UIRefactor/Product/Reputation/GiveReviewDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  GiveReviewDetailViewController.h
+//  Tokopedia
+//
+//  Created by Kenneth Vincent on 1/29/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GiveReviewDetailViewController : UIViewController
+
+@end
