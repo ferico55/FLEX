d13112ecc66b200fe25a43c3893ef3150b66082e
Tonito <tonito@tokopedia.com>
2016-02-17 16:09:26 +0700

    switch to live API and handling status code 206 partially content instead of 200 OK

--- Tokopedia/Classses/TKPStoreManager/TKPHomeBannerStore.m
+++ Tokopedia/Classses/TKPStoreManager/TKPHomeBannerStore.m
@@ -29,13 +29,13 @@ NSInteger static const TKPSuccessStatusCode = 200;
 
 
 - (void)fetchBannerWithCompletion:(void (^)(NSArray<Slide*>*, NSError *))completion {
-//    http://private-e80e2-tkpd.apiary-mock.com/v1/products/search?apple#
-    RKObjectManager *objectManager = [RKObjectManager sharedClient:@"http://private-e80e2-tkpd.apiary-mock.com/v1"];
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:[SliderObject mapping] method:RKRequestMethodGET pathPattern:@"products/search" keyPath:@"" statusCodes:[NSIndexSet indexSetWithIndex:TKPSuccessStatusCode]];
+    RKObjectManager *objectManager = [RKObjectManager sharedClient:@"https://mojito.tokopedia.com/api/v1"];
+
+    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:[SliderObject mapping] method:RKRequestMethodGET pathPattern:@"slides" keyPath:@"" statusCodes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(200, 299)]];
     [objectManager addResponseDescriptor:responseDescriptor];
 
-//    NSDictionary *parameters = @{@"page[size]" : @"25", @"filter[device]" : @"16", @"filter[target]" : @"65535", @"filter[state]" : @"1"};
-    RKObjectRequestOperation *operation = [objectManager appropriateObjectRequestOperationWithObject:nil method:RKRequestMethodGET path:@"products/search" parameters:nil];
+    NSDictionary *parameters = @{@"page[size]" : @"25", @"filter[device]" : @"16", @"filter[target]" : @"65535", @"filter[state]" : @"1"};
+    RKObjectRequestOperation *operation = [objectManager appropriateObjectRequestOperationWithObject:nil method:RKRequestMethodGET path:@"slides" parameters:[parameters autoParameters]];
 
     [operation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
         NSDictionary *result = [mappingResult dictionary];
@@ -56,13 +56,12 @@ NSInteger static const TKPSuccessStatusCode = 200;
 
 
 - (void)fetchMiniSlideWithCompletion:(void (^)(NSArray<MiniSlide *> *, NSError *))completion {
-    //    http://private-e80e2-tkpd.apiary-mock.com/v1/products/search?apple#
-    RKObjectManager *objectManager = [RKObjectManager sharedClient:@"http://private-e80e2-tkpd.apiary-mock.com/v1"];
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:[MiniSlideObject mapping] method:RKRequestMethodGET pathPattern:@"minislide" keyPath:@"" statusCodes:[NSIndexSet indexSetWithIndex:TKPSuccessStatusCode]];
+    RKObjectManager *objectManager = [RKObjectManager sharedClient:@"https://mojito.tokopedia.com/api/v1"];
+    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:[MiniSlideObject mapping] method:RKRequestMethodGET pathPattern:@"banners" keyPath:@"" statusCodes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(200, 299)]];
     [objectManager addResponseDescriptor:responseDescriptor];
     
-    //    NSDictionary *parameters = @{@"page[size]" : @"25", @"filter[device]" : @"16", @"filter[target]" : @"65535", @"filter[state]" : @"1"};
-    RKObjectRequestOperation *operation = [objectManager appropriateObjectRequestOperationWithObject:nil method:RKRequestMethodGET path:@"minislide" parameters:nil];
+    NSDictionary *parameters = @{@"page[size]" : @"25", @"filter[device]" : @"16", @"filter[target]" : @"65535", @"filter[state]" : @"1"};
+    RKObjectRequestOperation *operation = [objectManager appropriateObjectRequestOperationWithObject:nil method:RKRequestMethodGET path:@"banners" parameters:[parameters autoParameters]];
     
     [operation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
         NSDictionary *result = [mappingResult dictionary];
