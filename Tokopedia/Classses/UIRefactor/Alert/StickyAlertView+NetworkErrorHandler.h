2f8608dfc40c7220972e9a3c7f5b8aade135fb63
rennyrun <renny.runiawati@tokopedia.com>
2016-03-03 13:53:54 +0700

    create sticky alert view network error handler

---
+++ Tokopedia/Classses/UIRefactor/Alert/StickyAlertView+NetworkErrorHandler.h
@@ -0,0 +1,15 @@
+//
+//  StickyAlertView+NetworkErrorHandler.h
+//  Tokopedia
+//
+//  Created by Renny Runiawati on 3/3/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import "StickyAlertView.h"
+
+@interface StickyAlertView (NetworkErrorHandler)
+
++ (StickyAlertView*)showNetworkError:(NSError *)error;
+
+@end
