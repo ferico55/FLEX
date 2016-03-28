da3612718fd535764559b62ab8e9d5f37bb3b465
Johanes Effendi <johaneseffendi@Tokopedias-Mac-mini-2.local>
2016-03-28 11:25:06 +0700

    add entity and move mapping inside entity

---
+++ Tokopedia/Classses/UIRefactor/ShopPageView/ShopPageRequest.h
@@ -0,0 +1,13 @@
+//
+//  ShopPageRequest.h
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 3/28/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShopPageRequest : NSObject
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/ShopPageView/ShopPageRequest.m
@@ -0,0 +1,13 @@
+//
+//  ShopPageRequest.m
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 3/28/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import "ShopPageRequest.h"
+
+@implementation ShopPageRequest
+
+@end
