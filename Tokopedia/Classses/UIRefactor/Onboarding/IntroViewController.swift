e8cfafeb62a433877ee79e76fd06e3ac583b9bb7
Samuel Edwin <esam091@gmail.com>
2016-03-29 11:41:07 +0700

    Trying to use UIPageViewController back

--- Tokopedia/Classses/UIRefactor/Onboarding/IntroViewController.swift
+++ Tokopedia/Classses/UIRefactor/Onboarding/IntroViewController.swift
@@ -9,33 +9,51 @@
 import UIKit
 
 @objc
-class IntroViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class IntroViewController: UIPageViewController, UIPageViewControllerDataSource {
     
-    @IBOutlet var views: [UIView]!
-    @IBOutlet var collectionView: UICollectionView!
+    lazy var vcs: [UIViewController] = {
+        let vc1 = UIViewController()
+        
+        let vc2 = UIViewController()
+        
+        let vc3 = UIViewController()
+        
+        return [vc1, vc2, vc3, UIViewController(), UIViewController()]
+    }();
+    
+    init() {
+        super.init(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(white: 0xe7/255.0, alpha: 1)
+        self.dataSource = self
         
-        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "id")
+        setViewControllers([vcs[0]], direction: .Forward, animated: false, completion: nil)
+        
+        let pageControl = UIPageControl(frame: CGRect(x:50, y:50, width: 100, height: 50))
+        pageControl.tintColor = UIColor.redColor()
+        pageControl.numberOfPages = vcs.count
+        pageControl.currentPageIndicatorTintColor = UIColor.greenColor()
+        
+        
+        self.view.addSubview(pageControl)
+        
     }
     
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return views.count
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        let index = vcs.indexOf(viewController)! + 1
+        return index < vcs.count ? vcs[index]:nil
     }
     
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("id", forIndexPath: indexPath)
-        let page = views[indexPath.item]
-        
-        page.frame.size = self.view.bounds.size
-        cell.contentView.addSubview(page)
-        
-        return cell
-    }
-
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return self.view.bounds.size
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        let index = vcs.indexOf(viewController)! - 1
+        return index >= 0 ? vcs[index]:nil
     }
     
     override func didReceiveMemoryWarning() {
