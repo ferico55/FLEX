874bd7fcb366d10aed778bc600f672c6252657a7
Tonito <tonito@tokopedia.com>
2016-04-19 15:52:56 +0700

    add security view controller

---
+++ Tokopedia/Classses/UIRefactor/LoginSecurity.m
@@ -0,0 +1,13 @@
+//
+//  LoginSecurity.m
+//  Tokopedia
+//
+//  Created by Tonito Acen on 4/18/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import "LoginSecurity.h"
+
+@implementation LoginSecurity
+
+@end
