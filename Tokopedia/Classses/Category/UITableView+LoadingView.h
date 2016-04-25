d9e865762963a82aaa13caea2014abe9f7915657
feizalbadri <feizal.asmoro@tokopedia.com>
2016-04-25 13:22:55 +0700

    new category to show and hide activity indicator in table view

---
+++ Tokopedia/Classses/Category/UITableView+LoadingView.h
@@ -0,0 +1,16 @@
+//
+//  UITableView+LoadingView.h
+//  Tokopedia
+//
+//  Created by Tokopedia on 4/21/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (LoadingView)
+
+- (void)startIndicatorView;
+- (void)stopIndicatorView;
+
+@end
