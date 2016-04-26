c7e03976522f3846d16cb45e8f627a2a3e6fb510
Kenneth Vincent <kennethvincent@Kenneths-Mac-mini.local>
2016-04-26 10:52:50 +0700

    Add price alert request object

---
+++ Tokopedia/Classses/UIRefactor/PriceAlert/PriceAlertRequest.h
@@ -0,0 +1,13 @@
+//
+//  PriceAlertRequest.h
+//  Tokopedia
+//
+//  Created by Kenneth Vincent on 4/26/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PriceAlertRequest : NSObject
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/PriceAlert/PriceAlertRequest.m
@@ -0,0 +1,13 @@
+//
+//  PriceAlertRequest.m
+//  Tokopedia
+//
+//  Created by Kenneth Vincent on 4/26/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import "PriceAlertRequest.h"
+
+@implementation PriceAlertRequest
+
+@end
