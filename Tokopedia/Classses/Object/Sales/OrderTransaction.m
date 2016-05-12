e1e3e872e1058a4b0547750c8de4241405455717
Johanes Effendi <johaneseffendi@Joefs-Mac-mini.local>
2016-05-12 10:04:36 +0700

    fix wrong mapping in order detail

--- Tokopedia/Classses/Object/Sales/OrderTransaction.m
+++ Tokopedia/Classses/Object/Sales/OrderTransaction.m
@@ -12,7 +12,7 @@
 
 + (RKObjectMapping *)mapping {
     RKObjectMapping *mapping = [RKObjectMapping mappingForClass:[self class]];
-    [mapping addAttributeMappingsFromArray:@[@"order_JOB_status", @"order_auto_resi", @"order_auto_awb", @"order_products", @"order_history"]];
+    [mapping addAttributeMappingsFromArray:@[@"order_JOB_status", @"order_auto_resi", @"order_auto_awb", @"order_history"]];
     [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"order_customer" toKeyPath:@"order_customer" withMapping:[OrderCustomer mapping]]];
     [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"order_payment" toKeyPath:@"order_payment" withMapping:[OrderPayment mapping]]];
     [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"order_detail" toKeyPath:@"order_detail" withMapping:[OrderDetail mapping]]];
@@ -21,6 +21,7 @@
     [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"order_last" toKeyPath:@"order_last" withMapping:[OrderLast mapping]]];
     [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"order_destination" toKeyPath:@"order_destination" withMapping:[OrderDestination mapping]]];
     [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"order_shop" toKeyPath:@"order_shop" withMapping:[OrderSellerShop mapping]]];
+    [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"order_products" toKeyPath:@"order_products" withMapping:[OrderProduct mapping]]];
     return mapping;
 }
 
