8121853cfc96b377416d1640562ead257df11bd5
Samuel Edwin <esam091@gmail.com>
2016-05-30 10:31:08 +0700

    Remove unused code.

--- Tokopedia/Classses/UIRefactor/GeneralReport/ReportViewController.m
+++ Tokopedia/Classses/UIRefactor/GeneralReport/ReportViewController.m
@@ -20,13 +20,7 @@
 #import "ReviewRequest.h"
 
 @interface ReportViewController () <UITextViewDelegate, LoginViewDelegate> {
-    __weak RKObjectManager *_objectManager;
-    __weak RKManagedObjectRequestOperation *_request;
-    NSOperationQueue *_operationQueue;
-    NSTimer *_timer;
-    
     UserAuthentificationManager *_userManager;
-    ReviewRequest *_reviewRequest;
 }
 
 @property (weak, nonatomic) IBOutlet UITextView *messageTextView;
@@ -42,8 +36,7 @@
     
     self.title = @"Lapor";
     [self setTextViewPlaceholder:@"Isi deskripsi laporan kamu disini.."];
-    _operationQueue = [NSOperationQueue new];
-    
+
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Kirim"
                                                                    style:UIBarButtonItemStyleDone
                                                                   target:self
@@ -68,8 +61,6 @@
         [self.navigationController presentViewController:navigationController animated:YES completion:nil];
         return;
     }
-    
-    _reviewRequest = [ReviewRequest new];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -140,7 +131,6 @@
                 [stickyAlertView show];
             }
             else {
-                [self configureRestkit];
                 [self sendReport];
             }
             break;
@@ -151,157 +141,11 @@
 }
 
 
-- (void)configureRestkit {
-    _objectManager =  [RKObjectManager sharedClient];
-    
-    // setup object mappings
-    RKObjectMapping *statusMapping = [RKObjectMapping mappingForClass:[GeneralAction class]];
-    [statusMapping addAttributeMappingsFromDictionary:@{kTKPD_APISTATUSKEY:kTKPD_APISTATUSKEY,
-                                                        kTKPD_APIERRORMESSAGEKEY:kTKPD_APIERRORMESSAGEKEY,
-                                                        kTKPD_APISERVERPROCESSTIMEKEY:kTKPD_APISERVERPROCESSTIMEKEY}];
-    
-    RKObjectMapping *resultMapping = [RKObjectMapping mappingForClass:[GeneralActionResult class]];
-    [resultMapping addAttributeMappingsFromDictionary:@{kTKPD_APIISSUCCESSKEY:kTKPD_APIISSUCCESSKEY}];
-    
-    //relation
-    RKRelationshipMapping *resulRel = [RKRelationshipMapping relationshipMappingFromKeyPath:kTKPD_APIRESULTKEY toKeyPath:kTKPD_APIRESULTKEY withMapping:resultMapping];
-    [statusMapping addPropertyMapping:resulRel];
-    
-    
-    //register mappings with the provider using a response descriptor
-    RKResponseDescriptor *responseDescriptorStatus = [RKResponseDescriptor responseDescriptorWithMapping:statusMapping method:RKRequestMethodPOST
-                                                                                             pathPattern:[_delegate getPath] keyPath:@"" statusCodes:kTkpdIndexSetStatusCodeOK];
-    
-    [_objectManager addResponseDescriptor:responseDescriptorStatus];
-
-}
-
 - (void)sendReport {
-    if ([_delegate respondsToSelector:@selector(didFinishWritingReportWithReviewID:talkID:shopID:textMessage:)]) {
-        [_delegate didFinishWritingReportWithReviewID:_strReviewID
-                                               talkID:_strCommentTalkID
-                                               shopID:_strShopID
-                                          textMessage:_messageTextView.text];
-//        [self.navigationController popViewControllerAnimated:YES];
-    } else {
-        if(_request.isExecuting)return;
-        
-        NSMutableDictionary *param = [NSMutableDictionary new];
-        if([_delegate isMemberOfClass:[ProductReputationViewController class]] ||
-           [_delegate isMemberOfClass:[DetailMyReviewReputationViewController class]] ||
-           [_delegate isMemberOfClass:[ShopReviewPageViewController class]] ||
-           [_delegate isMemberOfClass:[MyReviewDetailViewController class]]) {
-            [param setObject:@"report_review" forKey:@"action"];
-            [param setObject:_strReviewID forKey:@"review_id"];
-            [param setObject:_strShopID forKey:@"shop_id"];
-        }
-        else {
-            [param addEntriesFromDictionary:(_strCommentTalkID==nil? [_delegate getParameter] :
-                                             @{@"action" : @"report_product_talk",
-                                               @"talk_id" : _strCommentTalkID?:@(0),
-                                               @"shop_id" : _strShopID? :@(0)
-                                               })];
-        }
-        
-        [param setObject:_messageTextView.text forKey:@"text_message"];
-        
-        if([_delegate isMemberOfClass:[ProductTalkViewController class]] ||
-           [_delegate isMemberOfClass:[InboxTalkViewController class]] ||
-           [_delegate isMemberOfClass:[ProductReputationViewController class]] ||
-           [_delegate isMemberOfClass:[DetailMyReviewReputationViewController class]]) {
-            NSString *tempProductID = strProductID;
-            if(tempProductID==nil || [_delegate isMemberOfClass:[ProductTalkViewController class]])
-                tempProductID = [((ProductTalkViewController *) _delegate).data objectForKey:kTKPD_PRODUCTIDKEY];
-            [param setObject:tempProductID forKey:kTKPD_PRODUCTIDKEY];
-        }
-        
-        _request = [_objectManager appropriateObjectRequestOperationWithObject:self method:RKRequestMethodPOST path:[_delegate getPath] parameters:[param encrypt]];
-        
-        [_request setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
-            NSLog(@"%@", operation.HTTPRequestOperation.responseString);
-            [self requestSuccess:mappingResult withOperation:operation];
-            [_timer invalidate];
-            _timer = nil;
-            
-        } failure:^(RKObjectRequestOperation *operation, NSError *error) {
-            NSLog(@"%@", operation.HTTPRequestOperation.responseString);
-            [_timer invalidate];
-            _timer = nil;
-            [self requestFail:error];
-            StickyAlertView *stickyError = [[StickyAlertView alloc] initWithErrorMessages:@[CStringErrorKirimLaporan] delegate:self];
-            [stickyError show];
-        }];
-        
-        [_operationQueue addOperation:_request];
-        
-        _timer= [NSTimer scheduledTimerWithTimeInterval:kTKPDREQUEST_TIMEOUTINTERVAL target:self selector:@selector(requestTimeout) userInfo:nil repeats:NO];
-        [[NSRunLoop currentRunLoop] addTimer:_timer forMode:NSRunLoopCommonModes];
-    }
-    
-}
-
-- (void)requestSuccess:(id)object withOperation:(RKObjectRequestOperation *)operation {
-    NSDictionary *result = ((RKMappingResult*)object).dictionary;
-    id stat = [result objectForKey:@""];
-    GeneralAction *generalaction = stat;
-    BOOL status = [generalaction.status isEqualToString:kTKPDREQUEST_OKSTATUS];
-    
-    if (status) {
-        [self requestProcess:object];
-    }
-}
-
-- (void)requestProcess:(id)object {
-    if (object) {
-        if ([object isKindOfClass:[RKMappingResult class]]) {
-            NSDictionary *result = ((RKMappingResult*)object).dictionary;
-            id stat = [result objectForKey:@""];
-            GeneralAction *generalaction = stat;
-            BOOL status = [generalaction.status isEqualToString:kTKPDREQUEST_OKSTATUS];
-            
-            if (status) {
-                if(generalaction.message_error)
-                {
-                    NSArray *array = generalaction.message_error?:[[NSArray alloc] initWithObjects:kTKPDMESSAGE_ERRORMESSAGEDEFAULTKEY, nil];
-                    StickyAlertView *alert = [[StickyAlertView alloc] initWithErrorMessages:array delegate:self];
-                    [alert show];
-                } else {
-                    //success
-                    if ([generalaction.result.is_success isEqualToString:@"1"]) {
-                        NSArray *array = generalaction.message_status?:[[NSArray alloc] initWithObjects:SUCCESS_REPORT_TALK, nil];
-                        StickyAlertView *stickyAlertView = [[StickyAlertView alloc] initWithSuccessMessages:array delegate:self];
-                        [stickyAlertView show];
-                        if ([_delegate isKindOfClass:[UINavigationController class]]) {
-                            UINavigationController *nav = (UINavigationController *)_delegate;
-                            [nav.navigationController popViewControllerAnimated:YES];
-                        }
-                        else if([_delegate isMemberOfClass:[ProductReputationViewController class]] || [_delegate isMemberOfClass:[DetailMyReviewReputationViewController class]] || [_delegate isMemberOfClass:[MyReviewDetailViewController class]]) {
-                            [self.navigationController popViewControllerAnimated:YES];
-                        }
-                    }
-                    else {
-                        StickyAlertView *stickyAlertView = [[StickyAlertView alloc] initWithErrorMessages:@[CStringFailedSendReport] delegate:self];
-                        [stickyAlertView show];
-                    }
-                }
-            }
-        }
-        else{
-            NSError *error = object;
-            if (!([error code] == NSURLErrorCancelled)){
-                NSString *errorDescription = error.localizedDescription;
-                UIAlertView *errorAlert = [[UIAlertView alloc]initWithTitle:ERROR_TITLE message:errorDescription delegate:self cancelButtonTitle:ERROR_CANCEL_BUTTON_TITLE otherButtonTitles:nil];
-                [errorAlert show];
-            }
-        }
-    }
-}
-
-- (void)requestFail:(id)error {
-    
-}
-
-- (void)requestTimeout {
+    [_delegate didFinishWritingReportWithReviewID:_strReviewID
+                                           talkID:_strCommentTalkID
+                                           shopID:_strShopID
+                                      textMessage:_messageTextView.text];
     
 }
 
