466143c11d6ed9a3da26a1e6d60cfb7a51d938b5
Tonito <tonito@tokopedia.com>
2016-06-10 10:41:14 +0700

    remove unwanted code

--- Tokopedia/Classses/UIRefactor/ViewModels/Product/ProductCell.m
+++ Tokopedia/Classses/UIRefactor/ViewModels/Product/ProductCell.m
@@ -12,13 +12,7 @@
 
 @implementation ProductCell
 
-- (void)awakeFromNib {
-    // Initialization code
-}
-
 - (void)setViewModel:(ProductModelView *)viewModel {
-//    [self.productName setText:viewModel.productName];
-    
     NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
     style.lineSpacing = 4.0;
     style.lineBreakMode = NSLineBreakByTruncatingTail;
@@ -27,7 +21,6 @@
                                  NSFontAttributeName            : [UIFont fontWithName:@"GothamMedium" size:12],
                                  NSParagraphStyleAttributeName  : style,
                                  NSForegroundColorAttributeName : [UIColor colorWithRed:10.0/255.0 green:126.0/255.0 blue:7.0/255.0 alpha:1],
-                                 
                                  };
     
     self.productName.attributedText = [[NSAttributedString alloc] initWithString:viewModel.productName attributes:attributes];
@@ -41,6 +34,7 @@
     if(!viewModel.productShop || [viewModel.productShop isEqualToString:@"0"]) {
         [self.productShop setHidden:YES];
     }
+    
     self.goldShopBadge.hidden = viewModel.isGoldShopProduct? NO : YES;
     self.luckyBadgePosition.constant = viewModel.isGoldShopProduct ? 1 : -15;
     self.preorderLabel.hidden = viewModel.isProductPreorder ? NO : YES;
@@ -49,29 +43,11 @@
     self.preorderPosition.constant = !viewModel.isWholesale ? -42 : 3;
     
     
-    NSURLRequest* request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:viewModel.productThumbUrl] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:kTKPDREQUEST_TIMEOUTINTERVAL];
-    
     [self.productImage setContentMode:UIViewContentModeCenter];
-    [self.productImage setImageWithURLRequest:request placeholderImage:[UIImage imageNamed:@"icon_toped_loading_grey-02.png"] success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
-        #pragma clang diagnostic push
-        #pragma clang diagnostic ignored "-Warc-retain-cycles"
-        [self.productImage setContentMode:UIViewContentModeScaleAspectFill];
-        [self.productImage setImage:image];
-    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
-        [self.productImage setImage:[UIImage imageNamed:@"icon_toped_loading_grey-02.png"]];
-    }];
+    [self.productImage setImageWithURL:[NSURL URLWithString:viewModel.productThumbUrl] placeholderImage:[UIImage imageNamed:@"icon_toped_loading_grey-02.png"]];
     
-    UIImageView *thumb =self.luckyMerchantBadge;
-    thumb.image = nil;
-    request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:viewModel.luckyMerchantImageURL] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:kTKPDREQUEST_TIMEOUTINTERVAL];
-    [self.luckyMerchantBadge setImageWithURLRequest:request placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-retain-cycles"
-        thumb.image = image;
-        thumb.contentMode = UIViewContentModeScaleAspectFill;
-    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
-        thumb.image = [UIImage imageNamed:@""];
-    }];
+    [self.luckyMerchantBadge setImageWithURL:[NSURL URLWithString:viewModel.luckyMerchantImageURL]];
+    [self.luckyMerchantBadge setContentMode:UIViewContentModeScaleAspectFill];
     
 }
 
@@ -84,21 +60,11 @@
     self.catalogPriceLabel.hidden = NO;
     self.catalogPriceLabel.text = viewModel.catalogPrice;
     
-    
     [self.productShop setText:[viewModel.catalogSeller isEqualToString:@"0"] ? @"Tidak ada penjual" : [NSString stringWithFormat:@"%@ Penjual", viewModel.catalogSeller]];
      self.goldShopBadge.hidden = YES;
     
-    NSURLRequest* request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:viewModel.catalogThumbUrl] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:kTKPDREQUEST_TIMEOUTINTERVAL];
-    
+    [self.productImage setImageWithURL:[NSURL URLWithString:viewModel.catalogThumbUrl] placeholderImage:[UIImage imageNamed:@"icon_toped_loading_grey-02.png"]];
     [self.productImage setContentMode:UIViewContentModeCenter];
-    [self.productImage setImageWithURLRequest:request placeholderImage:[UIImage imageNamed:@"icon_toped_loading_grey-02.png"] success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-retain-cycles"
-        [self.productImage setContentMode:UIViewContentModeScaleAspectFill];
-        [self.productImage setImage:image];
-    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
-        [self.productImage setImage:[UIImage imageNamed:@""]];
-    }];
     
     self.preorderLabel.hidden = YES;
     self.grosirLabel.hidden = YES;
