73135d4fe8f66997bd9fd73e815f71bf12dc6b9f
Tonito <tonito@tokopedia.com>
2016-06-10 10:37:46 +0700

    remove unused code

--- Tokopedia/Classses/UIRefactor/Search/SearchResult/SearchResultShopViewController.m
+++ Tokopedia/Classses/UIRefactor/Search/SearchResult/SearchResultShopViewController.m
@@ -11,82 +11,48 @@
 #import "sortfiltershare.h"
 
 #import "SearchItem.h"
-
 #import "TKPDTabNavigationController.h"
-
 #import "DetailProductViewController.h"
 #import "SearchResultShopCell.h"
 #import "SearchResultViewController.h"
 #import "SortViewController.h"
 #import "FilterViewController.h"
-#import "HotlistResultViewController.h"
 
 #import "SearchResultShopViewController.h"
-
 #import "NSDictionaryCategory.h"
 #import "TokopediaNetworkManager.h"
 #import "LoadingView.h"
 #import "NoResultReusableView.h"
-
-#import "URLCacheController.h"
 #import "ShopContainerViewController.h"
-#import "SpellCheckRequest.h"
 
 static NSString const *rows = @"12";
 
-@interface SearchResultShopViewController ()<UITableViewDelegate, UITableViewDataSource,SortViewControllerDelegate,FilterViewControllerDelegate, LoadingViewDelegate>
+@interface SearchResultShopViewController ()<UITableViewDelegate, UITableViewDataSource, FilterViewControllerDelegate, LoadingViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITableView *table;
-@property (strong, nonatomic) IBOutlet UIView *footer;
+@property (weak, nonatomic) IBOutlet UIView *footer;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *act;
-@property (strong, nonatomic) NSMutableArray *shops;
 @property (weak, nonatomic) IBOutlet UIView *shopview;
-@property (strong, nonatomic) SpellCheckRequest *spellCheckRequest;
 
 @end
 
 @implementation SearchResultShopViewController {
-    NSInteger _page;
-    NSInteger _limit;
+    NSInteger _start;
+    NoResultReusableView* _noResultView;
+    NSMutableArray* _shops;
     
-    NSInteger start;
-    NoResultReusableView *_noResultView;
-    
-    NSMutableArray *_urlarray;
+
     NSMutableDictionary *_params;
     
     NSString *_urinext;
-    NSString *_uriredirect;
-    
-    BOOL _isnodata;
-    BOOL _isrefreshview;
     
     UIRefreshControl *_refreshControl;
-    NSInteger _requestcount;
-    
-    SearchItem *_searchitem;
-    
-    __weak RKObjectManager *_objectmanager;
     TokopediaNetworkManager *tokopediaNetworkManager;
-    NSOperationQueue *_operationQueue;
-    
     
     LoadingView *loadingView;
     NSTimeInterval _timeinterval;
     
     NSIndexPath *_sortIndexPath;
-}
-
-#pragma mark - Initialization
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        _isrefreshview = NO;
-        _requestcount = 0;
-        _isnodata = YES;
-    }
-    return self;
 }
 
 - (void)initNoResultView{
@@ -100,31 +66,15 @@ static NSString const *rows = @"12";
 
 
 #pragma mark - Life Cycle
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     
-    /** create new **/
     _shops = [NSMutableArray new];
-    _urlarray = [NSMutableArray new];
     _params = [NSMutableDictionary new];
-    _operationQueue = [NSOperationQueue new];
-    
-    start = 0;
-    _limit = kTKPDSEARCH_LIMITPAGE;
-    
-    
-    /** set table view datasource and delegate **/
-    _table.delegate = self;
-    _table.dataSource = self;
+    _start = 0;
     
     [self initNoResultView];
-    
-    /** set table footer view (loading act) **/
-    if (_shops.count > 0) {
-        _isnodata = NO;
-    }
-    
+
     if (_data) {
         [_params addEntriesFromDictionary:_data];
     }
@@ -132,7 +82,6 @@ static NSString const *rows = @"12";
     _table.tableFooterView = _footer;
     [_act startAnimating];
     
-    /** adjust refresh control **/
     _refreshControl = [[UIRefreshControl alloc] init];
     _refreshControl.attributedTitle = [[NSAttributedString alloc] initWithString:kTKPDREQUEST_REFRESHMESSAGE];
     [_refreshControl addTarget:self action:@selector(refreshView:)forControlEvents:UIControlEventValueChanged];
@@ -141,10 +90,6 @@ static NSString const *rows = @"12";
     _shopview.hidden = YES;
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeCategory:) name:kTKPD_DEPARTMENTIDPOSTNOTIFICATIONNAMEKEY object:nil];
-    
-    _spellCheckRequest = [SpellCheckRequest new];
-    _spellCheckRequest.delegate = self;
-    
     
     UINib *cellNib = [UINib nibWithNibName:@"SearchResultShopCell" bundle:nil];
     [_table registerNib:cellNib forCellReuseIdentifier:@"SearchResultShopCellIdentifier"];
@@ -156,53 +101,33 @@ static NSString const *rows = @"12";
 }
 
 - (void)didReceiveShopResult:(SearchItem*)items{
-    NSString *statusstring = items.status;
-    BOOL status = [statusstring isEqualToString:kTKPDREQUEST_OKSTATUS];
-    
-    if (status) {
-        NSString *uriredirect = items.result.redirect_url;
-        
-        if (uriredirect == nil) {
-            if (start == 0) {
-                [_shops removeAllObjects];
-            }
-            
-            [_shops addObjectsFromArray:items.result.shops];
-            
-            if (_shops.count == 0) {
-                [_refreshControl endRefreshing];
-                
-                if([self isUsingAnyFilter]){
-                    [_noResultView setNoResultDesc:@"Silakan lakukan pencarian dengan filter lain"];
-                    [_noResultView hideButton:YES];
-                }else{
-                    [_noResultView setNoResultDesc:@"Silakan lakukan pencarian dengan kata kunci lain"];
-                    [_noResultView hideButton:YES];
-                }
-                
-                
-                [_table addSubview: _noResultView];
-            }else{
-                [_noResultView removeFromSuperview];
-                _urinext =  items.result.paging.uri_next;
-                [[NSNotificationCenter defaultCenter] postNotificationName:@"changeNavigationTitle" object:[_params objectForKey:@"search"]];
-                
-                start = [[tokopediaNetworkManager explodeURL:_urinext withKey:@"start"] integerValue];
-                _isnodata = NO;
-                
-                if([_urinext isEqualToString:@""]) {
-                    _table.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
-                }
-                
-                
-                
-            }
-        }
-        
-        [_table reloadData];
-        _shopview.hidden = NO;
+    if (_start == 0) {
+        [_shops removeAllObjects];
     }
-
+    
+    [_shops addObjectsFromArray:items.result.shops];
+    
+    if (_shops.count == 0) {
+        [_refreshControl endRefreshing];
+        [_noResultView setNoResultDesc:@"Toko yang Anda cari tidak di temukan. Silakan lakukan pencarian ulang"];
+        [_noResultView hideButton:YES];
+        
+        [_table addSubview: _noResultView];
+    } else {
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"changeNavigationTitle" object:[_params objectForKey:@"search"]];
+        
+        [_noResultView removeFromSuperview];
+        _urinext =  items.result.paging.uri_next;
+        _start = [[tokopediaNetworkManager explodeURL:_urinext withKey:@"start"] integerValue];
+        
+        if([_urinext isEqualToString:@""]) {
+            _table.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+        }
+    }
+    
+    
+    [_table reloadData];
+    _shopview.hidden = NO;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -219,8 +144,6 @@ static NSString const *rows = @"12";
 - (void)setData:(NSDictionary *)data {
     _data = data;
 }
-
-
 
 #pragma mark - Memory Management
 -(void)dealloc{
@@ -278,7 +201,7 @@ static NSString const *rows = @"12";
     [tokopediaNetworkManager requestWithBaseUrl:[NSString aceUrl]
                                            path:@"/search/v1/shop"
                                          method:RKRequestMethodGET
-                                      parameter:[self getParameter:nil]
+                                      parameter:[self parameters]
                                         mapping:[self mapping]
                                       onSuccess:^(RKMappingResult *successResult, RKObjectRequestOperation *operation) {
                                           [self didReceiveShopResult:[successResult.dictionary objectForKey:@""]];
@@ -290,19 +213,6 @@ static NSString const *rows = @"12";
 }
 
 
-#pragma mark - TKPDTabNavigationController Tap Button Notification
-
-- (IBAction)tapSortButton:(id)sender {
-    SortViewController *controller = [SortViewController new];
-    controller.selectedIndexPath = _sortIndexPath;
-    controller.delegate = self;
-    controller.sortType = SortShopSearch;
-    
-    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:controller];
-    [self.navigationController presentViewController:nav animated:YES completion:nil];
-}
-
-
 - (IBAction)tapFilterButton:(id)sender {
     FilterViewController *vc = [FilterViewController new];
     vc.data = @{kTKPDFILTER_DATAFILTERTYPEVIEWKEY:@(kTKPDFILTER_DATATYPESHOPVIEWKEY),
@@ -311,40 +221,6 @@ static NSString const *rows = @"12";
     UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:vc];
     [self.navigationController presentViewController:nav animated:YES completion:nil];
 }
-//
-//-(IBAction)tap:(id)sender
-//{
-//    UIButton *button = (UIButton *)sender;
-//    switch (button.tag) {
-//        case 10:
-//        {
-//            // Action Sort Button
-//            SortViewController *controller = [SortViewController new];
-//            controller.selectedIndexPath = _sortIndexPath;
-//            controller.delegate = self;
-//            controller.sortType = SortShopSearch;
-//            
-//            UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:controller];
-//            [self.navigationController presentViewController:nav animated:YES completion:nil];
-//            
-//            break;
-//        }
-//        case 11:
-//        {
-//            // Action Filter Button
-//            FilterViewController *vc = [FilterViewController new];
-//            vc.data = @{kTKPDFILTER_DATAFILTERTYPEVIEWKEY:@(kTKPDFILTER_DATATYPESHOPVIEWKEY),
-//                        kTKPDFILTER_DATAFILTERKEY: _params};
-//            vc.delegate = self;
-//            UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:vc];
-//            [self.navigationController presentViewController:nav animated:YES completion:nil];
-//            break;
-//        }
-//        default:
-//            break;
-//    }
-//}
-
 
 - (LoadingView *)getLoadView {
     if(loadingView == nil)
@@ -357,37 +233,12 @@ static NSString const *rows = @"12";
 }
 
 -(void)refreshView:(UIRefreshControl*)refresh {
-    _page = 1;
-    start = 0;
+    _start = 0;
     [_shops removeAllObjects];
     [_refreshControl endRefreshing];
     [self loadData];
 }
 
-- (BOOL) isUsingAnyFilter{
-    BOOL isUsingLocationFilter = [_params objectForKey:@"location"] != nil && ![[_params objectForKey:@"location"] isEqualToString:@""];
-    BOOL isUsingDepFilter = [_params objectForKey:@"department_id"] != nil && ![[_params objectForKey:@"department_id"] isEqualToString:@""];
-    BOOL isUsingPriceMinFilter = [_params objectForKey:@"price_min"] != nil && !([_params objectForKey:@"price_min"] == 0);
-    BOOL isUsingPriceMaxFilter = [_params objectForKey:@"price_max"] != nil && !([_params objectForKey:@"price_max"] == 0);
-    BOOL isUsingShopTypeFilter = [_params objectForKey:@"shop_type"] != nil && !([_params objectForKey:@"shop_type"] == 0);
-    
-    return  (isUsingDepFilter || isUsingLocationFilter || isUsingPriceMaxFilter || isUsingPriceMinFilter || isUsingShopTypeFilter);
-}
-
-#pragma mark - Sort Delegate
-- (void)didSelectSort:(NSString *)sort atIndexPath:(NSIndexPath *)indexPath {
-    [_params setObject:sort forKey:kTKPDSEARCH_APIORDERBYKEY];
-    [self refreshView:nil];
-    [_act startAnimating];
-    _table.tableFooterView = _footer;
-    _sortIndexPath = indexPath;
-}
-
--(void)SortViewController:(SortViewController *)viewController withUserInfo:(NSDictionary *)userInfo {
-    [_params addEntriesFromDictionary:userInfo];
-}
-
-#pragma mark - Filter Delegate
 -(void)FilterViewController:(FilterViewController *)viewController withUserInfo:(NSDictionary *)userInfo {
     [_params addEntriesFromDictionary:userInfo];
     [self refreshView:nil];
@@ -395,9 +246,7 @@ static NSString const *rows = @"12";
     _table.tableFooterView = _footer;
 }
 
-#pragma mark - Category notification
-- (void)changeCategory:(NSNotification *)notification
-{
+- (void)changeCategory:(NSNotification *)notification {
     [_shops removeAllObjects];
     [_params setObject:[notification.userInfo objectForKey:kTKPDSEARCH_APIDEPARTEMENTIDKEY] forKey:kTKPDSEARCH_APIDEPARTEMENTIDKEY];
     [self refreshView:nil];
@@ -411,38 +260,23 @@ static NSString const *rows = @"12";
 }
 
 
-#pragma mark - TokopediaNetworkManager Delegate
-- (NSDictionary*)getParameter:(int)tag {
+- (NSDictionary*)parameters {
     NSString *querry = [_params objectForKey:kTKPDSEARCH_DATASEARCHKEY];
-    NSString *deptid = [_params objectForKey:kTKPDSEARCH_APIDEPARTEMENTIDKEY];
+    NSString *categoryID = [_params objectForKey:kTKPDSEARCH_APIDEPARTEMENTIDKEY];
     
-    NSDictionary* param;
-
-    if (deptid == nil ) {
-        param = @{@"q"       :   querry?:@"",
-                  @"start" : @(start),
-                  @"rows" : rows,
-                  @"device" : @"ios",
-                  @"ob"     :   [_params objectForKey:kTKPDSEARCH_APIORDERBYKEY]?:@"",
-                  @"floc"    :   [_params objectForKey:kTKPDSEARCH_APILOCATIONKEY]?:@"",
-                  @"fshop"    :   [_params objectForKey:kTKPDSEARCH_APISHOPTYPEKEY]?:@"",
-                  @"pmin"    :   [_params objectForKey:kTKPDSEARCH_APIPRICEMINKEY]?:@"",
-                  @"pmax"    :   [_params objectForKey:kTKPDSEARCH_APIPRICEMAXKEY]?:@"",
-                  @"source" :   [_params objectForKey:@"search"]
-                  };
-    } else {
-        param = @{@"sc"   :   deptid?:@"",
-                  @"start" : @(start),
-                  @"rows" : rows,
-                  @"device" : @"ios",
-                  @"ob"         :   [_params objectForKey:kTKPDSEARCH_APIORDERBYKEY]?:@"",
-                  @"floc"        :   [_params objectForKey:kTKPDSEARCH_APILOCATIONKEY]?:@"",
-                  @"fshop"        :   [_params objectForKey:kTKPDSEARCH_APISHOPTYPEKEY]?:@"",
-                  @"pmin"        :   [_params objectForKey:kTKPDSEARCH_APIPRICEMINKEY]?:@"",
-                  @"pmax"        :   [_params objectForKey:kTKPDSEARCH_APIPRICEMAXKEY]?:@"",
-                  @"source" :   @"search"
-                  };
-    }
+    NSDictionary* param = @{
+                            @"sc" : categoryID?: @"",
+                            @"q" : (categoryID == nil) ? (querry ?:@"") : @"",
+                            @"start" : @(_start),
+                            @"rows" : rows,
+                            @"device" : @"ios",
+                            @"ob" : [_params objectForKey:kTKPDSEARCH_APIORDERBYKEY]?:@"",
+                            @"floc" : [_params objectForKey:kTKPDSEARCH_APILOCATIONKEY]?:@"",
+                            @"fshop" : [_params objectForKey:kTKPDSEARCH_APISHOPTYPEKEY]?:@"",
+                            @"pmin" : [_params objectForKey:kTKPDSEARCH_APIPRICEMINKEY]?:@"",
+                            @"pmax" : [_params objectForKey:kTKPDSEARCH_APIPRICEMAXKEY]?:@"",
+                            @"source" : @"search"
+                            };
     
     return param;
 }
