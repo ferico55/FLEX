bf198a6fc38a4ed96354f0f9a288bfbd341807b4
feizalbadri <feizal.asmoro@tokopedia.com>
2016-06-14 11:02:13 +0700

    setting paymet object

--- Tokopedia/Classses/Object/Shops/ShopSettings/Payment/SettingPaymentResult.m
+++ Tokopedia/Classses/Object/Shops/ShopSettings/Payment/SettingPaymentResult.m
@@ -10,4 +10,12 @@
 
 @implementation SettingPaymentResult
 
++ (RKObjectMapping *)mapping {
+    RKObjectMapping *mapping = [RKObjectMapping mappingForClass:[self class]];
+    [mapping addAttributeMappingsFromArray:@[@"note", @"loc", @"shop_id"]];
+    [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"shop_payment" toKeyPath:@"shop_payment" withMapping:[Payment mapping]]];
+    [mapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"payment_options" toKeyPath:@"payment_options" withMapping:[Payment mapping]]];
+    return mapping;
+}
+
 @end
