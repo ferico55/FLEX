27d83777b90508b3491186edfbb8fc1a10c19e49
Samuel Edwin <esam091@gmail.com>
2016-06-16 11:11:15 +0700

    Added CreatePasswordUserProfile

---
+++ Tokopedia/Classses/Object/Activation/Create Password/CreatePasswordUserProfile.swift
@@ -0,0 +1,14 @@
+//
+//  CreatePasswordUserProfile.swift
+//  Tokopedia
+//
+//  Created by Samuel Edwin on 6/16/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+import UIKit
+
+class CreatePasswordUserProfile: NSObject {
+    var name: String?
+    var email: String?
+}
