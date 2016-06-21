243b0bba11d6e141a4f0226b040e280db12a4efe
Samuel Edwin <esam091@gmail.com>
2016-06-21 13:55:24 +0700

    Remove unused method

--- Tokopedia/Classses/UIRefactor/TokopediaNetworkManager/TokopediaNetworkManager.h
+++ Tokopedia/Classses/UIRefactor/TokopediaNetworkManager/TokopediaNetworkManager.h
@@ -83,15 +83,6 @@ DEPRECATED_ATTRIBUTE
                   onSuccess:(void(^)(RKMappingResult* successResult, RKObjectRequestOperation* operation))successCallback
                   onFailure:(void(^)(NSError* errorResult)) errorCallback;
 
-- (void)requestNotObfuscatedWithBaseUrl:(NSString *)baseUrl
-                                   path:(NSString *)path
-                                 method:(RKRequestMethod)method
-                                 header:(NSDictionary<NSString *, NSString *> *)header
-                              parameter:(NSDictionary<NSString *,NSString *> *)parameter
-                                mapping:(RKObjectMapping *)mapping
-                              onSuccess:(void (^)(RKMappingResult *, RKObjectRequestOperation *))successCallback
-                              onFailure:(void (^)(NSError *))errorCallback;
-
 - (void) requestWithBaseUrl:(NSString*)baseUrl
                        path:(NSString*)path
                      method:(RKRequestMethod)method
