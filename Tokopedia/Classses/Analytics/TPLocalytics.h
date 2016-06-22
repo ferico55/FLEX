b52eb0afe9a032498ff9039c9ba1abbf19e47e4b
feizalbadri <feizal.asmoro@tokopedia.com>
2016-06-22 15:53:07 +0700

    remove ATCPushLocalytics add new file that contains all localytics method

---
+++ Tokopedia/Classses/Analytics/TPLocalytics.h
@@ -0,0 +1,13 @@
+//
+//  TPLocalytics.h
+//  Tokopedia
+//
+//  Created by Tokopedia on 6/22/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPLocalytics : NSObject
+
+@end
---
+++ Tokopedia/Classses/Analytics/TPLocalytics.m
@@ -0,0 +1,13 @@
+//
+//  TPLocalytics.m
+//  Tokopedia
+//
+//  Created by Tokopedia on 6/22/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import "TPLocalytics.h"
+
+@implementation TPLocalytics
+
+@end
