bb2f3762181d1ec434b5037cc53c9416fae49011
Tedo Pranowo <tedo.pranowo@tokopedia.com>
2016-06-28 10:04:32 +0700

    Refactor RequestAddress.m

--- Tokopedia/Classses/UIRefactor/Address/RequestAddress.m
+++ Tokopedia/Classses/UIRefactor/Address/RequestAddress.m
@@ -13,10 +13,6 @@
 #define TagRequestGetCity 11
 #define TagRequestGetDistrict 12
 
-@interface RequestAddress ()<TokopediaNetworkManagerDelegate>
-
-@end
-
 @implementation RequestAddress
 {
     TokopediaNetworkManager *_provinceNetworkManager;
@@ -26,45 +22,17 @@
     AddressObj *_address;
     
     NSString *_cachePath;
-    NSTimeInterval _timeinterval;
     URLCacheConnection *_cacheConnection;
     URLCacheController *_cacheController;
 }
 
-
--(TokopediaNetworkManager*)provinceNetworkManager
-{
-    if (!_provinceNetworkManager) {
-        _provinceNetworkManager = [TokopediaNetworkManager new];
-        _provinceNetworkManager.delegate = self;
-        _provinceNetworkManager.tagRequest = TagRequestGetProvince;
+-(TokopediaNetworkManager*)setNetworkManager:(TokopediaNetworkManager*)networkManager withTag:(int)tag {
+    if (!networkManager){
+        networkManager = [TokopediaNetworkManager new];
+        networkManager.tagRequest = tag;
         [self initCache];
     }
-    
-    return _provinceNetworkManager;
-}
-
--(TokopediaNetworkManager*)cityNetworkManager
-{
-    if (!_cityNetworkManager) {
-        _cityNetworkManager = [TokopediaNetworkManager new];
-        _cityNetworkManager.delegate = self;
-        _cityNetworkManager.tagRequest = TagRequestGetCity;
-        [self initCache];
-    }
-    return _cityNetworkManager;
-}
-
--(TokopediaNetworkManager*)districtNetworkManager
-{
-    if (!_districtNetworkManager) {
-        _districtNetworkManager = [TokopediaNetworkManager new];
-        _districtNetworkManager.delegate = self;
-        _districtNetworkManager.tagRequest = TagRequestGetDistrict;
-        [self initCache];
-    }
-    
-    return _districtNetworkManager;
+    return networkManager;
 }
 
 - (void)initCache {
@@ -101,31 +69,42 @@
 
 -(void)doRequestProvinces
 {
-    TokopediaNetworkManager *networkManager = [self provinceNetworkManager];
-    [self doRequestNetworkManager:networkManager];
+    [self setNetworkManager:_provinceNetworkManager withTag:TagRequestGetProvince];
+    [self doRequestNetworkManager:_provinceNetworkManager];
 }
 
 -(void)doRequestCities
 {
-    TokopediaNetworkManager *networkManager = [self cityNetworkManager];
-    [self doRequestNetworkManager:networkManager];
+    [self setNetworkManager:_cityNetworkManager withTag:TagRequestGetCity];
+    [self doRequestNetworkManager:_cityNetworkManager];
 }
 
 -(void)doRequestDistricts
 {
-    TokopediaNetworkManager *networkManager = [self districtNetworkManager];
-    [self doRequestNetworkManager:networkManager];
+    [self setNetworkManager:_districtNetworkManager withTag:TagRequestGetDistrict];
+    [self doRequestNetworkManager:_districtNetworkManager];
 }
 
 -(void)doRequestNetworkManager:(TokopediaNetworkManager*)networkManager;
 {
-    [_cacheController getFileModificationDate];
-    _timeinterval = fabs([_cacheController.fileDate timeIntervalSinceNow]);
+    RKMappingResult* cacheMappingResult = [self getFromCache];
     
-    if([self getFromCache]) {
-        [networkManager requestSuccess:[self getFromCache] withOperation:nil];
+    if(cacheMappingResult) {
+        NSDictionary *resultDict = cacheMappingResult.dictionary;
+        _address = [resultDict objectForKey:@""];
+        [_delegate successRequestAddress:self withResultObj:_address];
     } else {
-        [networkManager doRequest];
+        [networkManager requestWithBaseUrl:[NSString basicUrl]
+                                      path:@"address.pl"
+                                    method:RKRequestMethodPOST
+                                 parameter:[self getParameter:networkManager.tagRequest]
+                                   mapping:[AddressObj mapping]
+                                 onSuccess:^(RKMappingResult *successResult, RKObjectRequestOperation *operation) {
+                                     [self actionAfterSuccessfulRequestWithResult:successResult withOperation:operation];
+                                 }
+                                 onFailure:^(NSError *errorResult) {
+                                     [self actionAfterError:errorResult];
+                                 }];
     }
 }
 
@@ -153,55 +132,11 @@
     return param;
 }
 
--(id)getObjectManager:(int)tag
+-(id)getObjectManager
 {
     RKObjectManager *objecManager = [RKObjectManager sharedClient];
     
-    RKObjectMapping *statusMapping = [RKObjectMapping mappingForClass:[AddressObj class]];
-    [statusMapping addAttributeMappingsFromDictionary:@{kTKPD_APIERRORMESSAGEKEY:kTKPD_APIERRORMESSAGEKEY,
-                                                        kTKPD_APISTATUSKEY:kTKPD_APISTATUSKEY,
-                                                        kTKPD_APISERVERPROCESSTIMEKEY:kTKPD_APISERVERPROCESSTIMEKEY}];
-    
-    RKObjectMapping *resultMapping = [RKObjectMapping mappingForClass:[AddressResult class]];
-    
-    RKObjectMapping *citiesMapping = [RKObjectMapping mappingForClass:[AddressCity class]];
-    [citiesMapping addAttributeMappingsFromArray:@[@"city_id",
-                                                 @"city_name"
-                                                 ]
-     ];
-    
-    RKObjectMapping *districtsMapping = [RKObjectMapping mappingForClass:[AddressDistrict class]];
-    [districtsMapping addAttributeMappingsFromArray:@[@"district_id",
-                                                      @"district_name"
-                                                      ]
-     ];
-    
-    RKObjectMapping *provincesMapping = [RKObjectMapping mappingForClass:[AddressProvince class]];
-    [provincesMapping addAttributeMappingsFromArray:@[@"province_id",
-                                                      @"province_name"
-                                                      ]
-     ];
-    
-    // Relationship Mapping
-    [statusMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:kTKPD_APIRESULTKEY toKeyPath:kTKPD_APIRESULTKEY withMapping:resultMapping]];
-    
-    RKRelationshipMapping *cityRelMapping = [RKRelationshipMapping relationshipMappingFromKeyPath:@"cities"
-                                                                                        toKeyPath:@"cities"
-                                                                                      withMapping:citiesMapping];
-    [resultMapping addPropertyMapping:cityRelMapping];
-    
-    RKRelationshipMapping *districtRelMapping = [RKRelationshipMapping relationshipMappingFromKeyPath:@"districts"
-                                                                                        toKeyPath:@"districts"
-                                                                                      withMapping:districtsMapping];
-    [resultMapping addPropertyMapping:districtRelMapping];
-    
-    RKRelationshipMapping *provinceRelMapping = [RKRelationshipMapping relationshipMappingFromKeyPath:@"provinces"
-                                                 
-                                                                                            toKeyPath:@"provinces"
-                                                                                          withMapping:provincesMapping];
-    [resultMapping addPropertyMapping:provinceRelMapping];
-    
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:statusMapping
+    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:[AddressObj mapping]
                                                                                             method:RKRequestMethodPOST
                                                                                        pathPattern:@"address.pl"
                                                                                            keyPath:@""
@@ -212,22 +147,11 @@
     return objecManager;
 }
 
--(NSString *)getRequestStatus:(id)result withTag:(int)tag
+-(void)actionAfterSuccessfulRequestWithResult:(RKMappingResult *)successResult withOperation:(RKObjectRequestOperation *)operation
 {
-    NSDictionary *resultDict = ((RKMappingResult*)result).dictionary;
-    id info = [resultDict objectForKey:@""];
-    _address = info;
+    NSDictionary *resultDict = successResult.dictionary;
+    _address = [resultDict objectForKey:@""];
     
-    return _address.status;
-}
-
--(NSString *)getPath:(int)tag
-{
-    return @"address.pl";
-}
-
--(void)actionAfterRequest:(id)successResult withOperation:(RKObjectRequestOperation *)operation withTag:(int)tag
-{
     if (_address.message_error.count == 0) {
         [self setToCache:operation];
         [_delegate successRequestAddress:self withResultObj:_address];
@@ -239,31 +163,20 @@
     }
 }
 
--(void)actionFailAfterRequest:(id)errorResult withTag:(int)tag
-{
-    NSError *error = errorResult;
+-(void)actionAfterError:(NSError *)errorResult{
     NSArray *errors;
-    if(error.code == -1011) {
+    if(errorResult.code == -1011) {
         errors = @[@"Mohon maaf, terjadi kendala pada server"];
-    } else if (error.code==-1009 || error.code==-999) {
+    } else if (errorResult.code==-1009 || errorResult.code==-999) {
         errors = @[@"Tidak ada koneksi internet"];
     } else {
-        errors = @[error.localizedDescription];
+        errors = @[errorResult.localizedDescription];
     }
     
     [_delegate failedRequestAddress:errors];
 }
 
--(void)actionAfterFailRequestMaxTries:(int)tag
-{
-
-}
-
-
-- (id)getFromCache {
-    [_cacheController getFileModificationDate];
-    _timeinterval = fabs([_cacheController.fileDate timeIntervalSinceNow]);
-    
+- (RKMappingResult*)getFromCache {
     NSError* error;
     NSData *data = [NSData dataWithContentsOfFile:_cachePath];
     
@@ -276,7 +189,7 @@
         }
         
         NSMutableDictionary *mappingsDictionary = [[NSMutableDictionary alloc] init];
-        for (RKResponseDescriptor *descriptor in ((RKObjectManager*)[self getObjectManager:_tag]).responseDescriptors) {
+        for (RKResponseDescriptor *descriptor in ((RKObjectManager*)[self getObjectManager]).responseDescriptors) {
             [mappingsDictionary setObject:descriptor.mapping forKey:descriptor.keyPath];
         }
         
