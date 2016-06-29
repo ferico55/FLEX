49abc02f1443f5986ec9f8637dc48daccb2f8bce
Johanes Effendi <johaneseffendi@Joefs-Mac-mini.local>
2016-06-29 10:21:37 +0700

    remove price and weight var key

--- Tokopedia/Classses/UIRefactor/Sales/NewOrder/RejectOrder/RejectReasonEditPriceViewController.m
+++ Tokopedia/Classses/UIRefactor/Sales/NewOrder/RejectOrder/RejectReasonEditPriceViewController.m
@@ -16,9 +16,6 @@
 
 @interface RejectReasonEditPriceViewController ()<UITableViewDelegate, UITableViewDataSource, TKPDAlertViewDelegate, UIScrollViewDelegate>
 
-@property (strong, nonatomic) NSString *priceKey;
-@property (strong, nonatomic) NSString *weightKey;
-
 @property (strong, nonatomic) IBOutlet UITextField *priceTextField;
 @property (strong, nonatomic) IBOutlet UITextField *weightTextField;
 @property (strong, nonatomic) IBOutlet UILabel *productName;
@@ -242,12 +239,12 @@
 -(void)alertView:(TKPDAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
     if(alertView.tag == CURRENCY_PICKER){
         NSInteger index = [[alertView.data objectForKey:DATA_INDEX_KEY] integerValue];
-        _priceKey = [ARRAY_PRICE_CURRENCY[index] objectForKey:DATA_VALUE_KEY];
+        _orderProduct.product_price_currency = [ARRAY_PRICE_CURRENCY[index] objectForKey:DATA_VALUE_KEY];
         NSString *name = [ARRAY_PRICE_CURRENCY[index] objectForKey:DATA_NAME_KEY];
         _currencyLabel.text = name;
     }else if(alertView.tag == WEIGHT_PICKER){
         NSInteger index = [[alertView.data objectForKey:DATA_INDEX_KEY] integerValue];
-        _weightKey = [ARRAY_WEIGHT_UNIT[index] objectForKey:DATA_VALUE_KEY];
+        _orderProduct.product_weight_unit = [ARRAY_WEIGHT_UNIT[index] objectForKey:DATA_VALUE_KEY];
         NSString *name = [ARRAY_WEIGHT_UNIT[index] objectForKey:DATA_NAME_KEY];
         _weightLabel.text = name;
     }
