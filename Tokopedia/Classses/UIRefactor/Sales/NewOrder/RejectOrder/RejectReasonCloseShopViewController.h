f623ffb88c06fb424f7375e14b000ad0d5a39a66
Johanes Effendi <johaneseffendi@Joefs-Mac-mini.local>
2016-06-30 15:23:30 +0700

    add rejectreasoncloseshop

---
+++ Tokopedia/Classses/UIRefactor/Sales/NewOrder/RejectOrder/RejectReasonCloseShopViewController.h
@@ -0,0 +1,13 @@
+//
+//  RejectReasonCloseShopViewController.h
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 6/30/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RejectReasonCloseShopViewController : UIViewController
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/Sales/NewOrder/RejectOrder/RejectReasonCloseShopViewController.m
@@ -0,0 +1,37 @@
+//
+//  RejectReasonCloseShopViewController.m
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 6/30/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import "RejectReasonCloseShopViewController.h"
+
+@interface RejectReasonCloseShopViewController ()
+
+@end
+
+@implementation RejectReasonCloseShopViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
