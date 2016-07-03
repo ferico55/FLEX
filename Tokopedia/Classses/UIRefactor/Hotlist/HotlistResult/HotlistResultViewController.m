cff3c21ed652dfb2f3fde6d6061372511c815121
rennyrun <renny.runiawati@tokopedia.com>
2016-07-04 01:44:29 +0700

    remove default sort at hot product. get default sort from hotlist banner

--- Tokopedia/Classses/UIRefactor/Hotlist/HotlistResult/HotlistResultViewController.m
+++ Tokopedia/Classses/UIRefactor/Hotlist/HotlistResult/HotlistResultViewController.m
@@ -166,8 +166,6 @@ static NSString const *rows = @"12";
     [super viewDidLoad];
     _page = 0;
     
-    [self setDefaultSort];
-    
     if (![self isUseDynamicFilter]) {
         [self setRightButton];
     }
@@ -269,28 +267,6 @@ static NSString const *rows = @"12";
         self.screenName = @"Hot List Detail";
         [TPAnalytics trackScreenName:@"Hot List Detail" gridType:self.cellType];
     }
-}
-
--(void)setDefaultSort{
-    [_detailfilter setObject:[self defaultSortID] forKey:[self defaultSortKey]];
-    _selectedSort = [self defaultSortDynamicFilter];
-    _selectedSortParam = @{[self defaultSortKey]:[self defaultSortID]};
-    
-}
-
--(ListOption*)defaultSortDynamicFilter{
-    ListOption *sort = [ListOption new];
-    sort.value = [self defaultSortID];
-    sort.key = [self defaultSortKey];
-    return sort;
-}
-
--(NSString*)defaultSortKey{
-    return @"ob";
-}
-
--(NSString*)defaultSortID{
-    return @"1";
 }
 
 - (void)registerAllNib {
@@ -640,7 +616,7 @@ static NSString const *rows = @"12";
 #pragma mark - Sort Delegate
 - (void)didSelectSort:(NSString *)sort atIndexPath:(NSIndexPath *)indexPath {
     _sortIndexPath = indexPath;
-    [_detailfilter setObject:sort forKey:[self defaultSortKey]];
+    [_detailfilter setObject:sort forKey:sort];
     [self refreshView:nil];
 }
 
@@ -894,7 +870,7 @@ static NSString const *rows = @"12";
     NSDictionary *query = @{
         @"negative_keyword" : q.negative_keyword?:@"",
         @"sc" : q.sc?:@"",
-        @"ob" : q.ob?:[self defaultSortID],
+        @"ob" : q.ob?:@"",
         @"terms" : q.terms?:@"",
         @"fshop" : q.fshop?:@"",
         @"q" : q.q?:@"",
@@ -906,7 +882,6 @@ static NSString const *rows = @"12";
     _rootCategoryID = q.sc;
     [_detailfilter addEntriesFromDictionary:query];
     _selectedFilterParam = query;
-    [self setDefaultSort];
     
     _start = 0;
     [self requestHotlist];
