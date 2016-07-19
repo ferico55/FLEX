f681d4eda3e9ad3f82aa6012794ff4046235e44b
rennyrun <renny.runiawati@tokopedia.com>
2016-07-19 17:18:27 +0700

    set default category only if redirect

--- Tokopedia/Classses/UIRefactor/Search/SearchResult/SearchResultViewController.m
+++ Tokopedia/Classses/UIRefactor/Search/SearchResult/SearchResultViewController.m
@@ -1161,7 +1161,6 @@ ImageSearchRequestDelegate
     
     NSString *redirect_url = search.data.redirect_url;
     if(search.data.department_id && ![search.data.department_id isEqualToString:@"0"]) {
-        _rootCategoryID = ([_rootCategoryID integerValue] == 0)?search.data.department_id:_rootCategoryID;
         NSString *departementID = search.data.department_id?:@"";
         [_params setObject:departementID forKey:@"sc"];
         NSString *departementName = [_params objectForKey:@"department_name"]?:@"";
@@ -1259,7 +1258,7 @@ ImageSearchRequestDelegate
             [_collectionView reloadData];
         }
     } else {
-        
+        _rootCategoryID = ([_rootCategoryID integerValue] == 0)?search.data.department_id:_rootCategoryID;
         NSURL *url = [NSURL URLWithString:search.data.redirect_url];
         NSArray* query = [[url path] componentsSeparatedByString: @"/"];
         
