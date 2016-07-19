9915914bec4fc45eafc0981ad3541a1aee1ea158
Tonito <tonito@tokopedia.com>
2016-07-19 16:32:08 +0700

    Add extractTKPMEUrl to differentiate real stringReplaceAHref for tkp.me only purpose

---
+++ Tokopedia/Classses/Object/DetailProduct/ProductReturnInfo.swift
@@ -0,0 +1,13 @@
+//
+//  ProductReturnInfo.swift
+//  Tokopedia
+//
+//  Created by Tonito Acen on 7/19/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+import UIKit
+
+class ProductReturnInfo: NSObject {
+
+}
