0bf68f5b3698054ddc271a440c6c22d7d8262acb
Kenneth Vincent <kennethvincent@Kenneths-Mac-mini.local>
2016-08-03 13:36:00 +0700

    Delete .m.orig files and fix syntax error

--- Tokopedia/Classses/UIRefactor/Product/ProductAddEditDetailViewController.m
+++ Tokopedia/Classses/UIRefactor/Product/ProductAddEditDetailViewController.m
@@ -411,7 +411,7 @@ NSString * const ProductStatusWarehouse = @"3";
             else if (indexPath.row == BUTTON_PRODUCT_ETALASE_DETAIL)
             {
                 //cell.detailTextLabel.textColor = (isProductWarehouse)?[UIColor grayColor]:[UIColor colorWithRed:(0.f/255.f) green:122.f/255.f blue:255.f/255.f alpha:1];
-                NSString etalaseTitle;
+                NSString *etalaseTitle;
                 if([product.product_etalase isEqualToString:@""] ||
                    [product.product_etalase isEqualToString:@"0"] ||
                    !product.product_etalase) {
