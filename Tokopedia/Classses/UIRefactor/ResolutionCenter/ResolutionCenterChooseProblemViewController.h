4dfbfcba7df4eeb10eb7d451f307057a0c4d1b83
Johanes Effendi <johaneseffendi@Joefs-Mac-mini.local>
2016-08-04 13:18:12 +0700

    finished creating UI for create step one

---
+++ Tokopedia/Classses/UIRefactor/ResolutionCenter/ResolutionCenterChooseProblemViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResolutionCenterChooseProblemViewController.h
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 8/3/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResolutionCenterChooseProblemViewController : UIViewController
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/ResolutionCenter/ResolutionCenterChooseProblemViewController.m
@@ -0,0 +1,37 @@
+//
+//  ResolutionCenterChooseProblemViewController.m
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 8/3/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import "ResolutionCenterChooseProblemViewController.h"
+
+@interface ResolutionCenterChooseProblemViewController ()
+
+@end
+
+@implementation ResolutionCenterChooseProblemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
