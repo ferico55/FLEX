0cc986eed1d0ccd0679afd4d63075b4ecd192281
Johanes Effendi <johaneseffendi@Joefs-Mac-mini.local>
2016-08-04 16:59:58 +0700

    create unfinished step two cell

---
+++ Tokopedia/Classses/UIRefactor/ResolutionCenter/ResolutionCenterCreateStepTwoCell.h
@@ -0,0 +1,13 @@
+//
+//  ResolutionCenterCreateStepTwoCell.h
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 8/4/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResolutionCenterCreateStepTwoCell : UITableViewCell
+
+@end
---
+++ Tokopedia/Classses/UIRefactor/ResolutionCenter/ResolutionCenterCreateStepTwoCell.m
@@ -0,0 +1,24 @@
+//
+//  ResolutionCenterCreateStepTwoCell.m
+//  Tokopedia
+//
+//  Created by Johanes Effendi on 8/4/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import "ResolutionCenterCreateStepTwoCell.h"
+
+@implementation ResolutionCenterCreateStepTwoCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
