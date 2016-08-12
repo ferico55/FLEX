4b179752c3f23745dbea61bc03f93585c5f23666
Johanes Effendi <johaneseffendi@Joefs-Mac-mini.local>
2016-08-12 14:08:22 +0700

    add tableview delegate and datasource to step 3

--- Tokopedia/Classses/UIRefactor/ResolutionCenter/ResolutionCenterCreateStepThreeViewController.m
+++ Tokopedia/Classses/UIRefactor/ResolutionCenter/ResolutionCenterCreateStepThreeViewController.m
@@ -8,7 +8,13 @@
 
 #import "ResolutionCenterCreateStepThreeViewController.h"
 
-@interface ResolutionCenterCreateStepThreeViewController ()
+@interface ResolutionCenterCreateStepThreeViewController ()<UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate>
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) IBOutlet UITableViewCell *solutionCell;
+@property (strong, nonatomic) IBOutlet UITableViewCell *refundCell;
+@property (strong, nonatomic) IBOutlet UITableViewCell *photoCell;
+
+@property (strong, nonatomic) IBOutlet UIButton *uploadButtons;
 
 @end
 
@@ -16,7 +22,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +31,66 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - TableView
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.section == 0){
+        if(indexPath.row == 0){
+            return _solutionCell;
+        }else{
+            return _refundCell;
+        }
+    }else{
+        return _photoCell;
+    }
 }
-*/
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if(section == 0){
+        return 2;
+    }else{
+        return 1;
+    }
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(indexPath.section == 0){
+        if(indexPath.row == 0){
+            return _solutionCell.frame.size.height;
+        }else{
+            return _refundCell.frame.size.height;
+        }
+    }else{
+        return _photoCell.frame.size.height;
+    }
+}
+
+-(UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView *header = nil;
+    if(section == 0){
+        header = [[UIView alloc]initWithFrame:CGRectMake(16, 28, 320, 40)];
+        header.backgroundColor = [UIColor clearColor];
+        
+        UILabel *lbl = [[UILabel alloc]initWithFrame:header.frame];
+        lbl.backgroundColor = [UIColor clearColor];
+        lbl.text = @"Masalah pada barang yang Anda terima";
+        lbl.textAlignment = NSTextAlignmentLeft;
+        lbl.font = [UIFont fontWithName:@"Gotham Book" size:12.0];
+        [lbl setNumberOfLines:0];
+        [lbl sizeToFit];
+        [header addSubview:lbl];
+    }
+    return header;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    if(section == 0){
+        return 50;
+    }
+    return 0;
+}
 
 @end
