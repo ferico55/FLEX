c19e50eb730c07a1620c4f70f16284fc03ded282
Samuel Edwin <esam091@gmail.com>
2016-08-15 13:14:41 +0700

    Add font theme

---
+++ Tokopedia/Classses/Category/UIFont+Theme.h
@@ -0,0 +1,24 @@
+//
+//  UIFont+Theme.h
+//  Tokopedia
+//
+//  Created by Samuel Edwin on 8/15/16.
+//  Copyright © 2016 TOKOPEDIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIFont(Theme)
+
++ (UIFont *)title1Theme;
++ (UIFont *)title1ThemeMedium;
++ (UIFont *)title2Theme;
++ (UIFont *)title2ThemeMedium;
++ (UIFont *)largeTheme;
++ (UIFont *)largeThemeMedium;
++ (UIFont *)smallTheme;
++ (UIFont *)smallThemeMedium;
++ (UIFont *)microTheme;
++ (UIFont *)microThemeMedium;
+
+@end
