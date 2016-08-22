2740b571b89624102f3eae8bb17f4ac63aa0c45d
rennyrun <renny.runiawati@tokopedia.com>
2016-08-22 19:38:05 +0700

    set default returnable 0

--- Tokopedia/Classses/Object/ProductEditDetail.swift
+++ Tokopedia/Classses/Object/ProductEditDetail.swift
@@ -25,7 +25,7 @@ class ProductEditDetail: NSObject {
     var product_status: String = ""
     var product_short_desc: String = ""
     var product_weight_unit_name: String = ""
-    var product_returnable: String = ""
+    var product_returnable: String = "0"
     var product_catalog: CatalogList = CatalogList()
     var product_category :CategoryDetail = CategoryDetail()
     
