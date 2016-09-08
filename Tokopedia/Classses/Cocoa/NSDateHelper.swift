7126c4ff62a4e522b1cb3dd68f478f34f7086d43
rennyrun <renny.runiawati@tokopedia.com>
2016-09-08 14:31:33 +0700

    fix crash date formatter

--- Tokopedia/Classses/Cocoa/NSDateHelper.swift
+++ Tokopedia/Classses/Cocoa/NSDateHelper.swift
@@ -11,6 +11,12 @@ import Foundation
 extension NSDate {
     class func convertDateString(string: String, fromFormat format1: String, toFormat format2: String ) -> String{
         let dtf: NSDateFormatter = NSDateFormatter()
+
+        //Need to add local and timezone for iOS above 8.1
+        let Locale:NSLocale=NSLocale(localeIdentifier: "id_ID")
+        dtf.locale=Locale
+        dtf.timeZone = NSTimeZone(name: "GMT")
+        
         dtf.dateFormat = format1
         let date: NSDate = dtf.dateFromString(string)!
         let dateFormat: NSDateFormatter = NSDateFormatter()
