c6df29f1500be54823e6bcd9b07a2895f292799a
Samuel Edwin <esam091@gmail.com>
2016-10-25 11:00:39 +0700

    Put five tabs

--- Tokopedia/Classses/UIRefactor/ShopPageView/ShopTabView.mm
+++ Tokopedia/Classses/UIRefactor/ShopPageView/ShopTabView.mm
@@ -26,14 +26,145 @@
     return self;
 }
 
++ (CKComponent *)verticalSeparator {
+    return [CKInsetComponent
+            newWithInsets:UIEdgeInsetsMake(10, 0, 10, 0)
+            component:
+            [CKComponent
+             newWithView:{
+                 [UIView class],
+                 {{@selector(setBackgroundColor:), [UIColor colorWithWhite:0.937 alpha:1.00]}}
+             }
+             size:{.width = 1}]];
+
+}
+
 + (CKComponent *)componentForModel:(id<NSObject>)model
                            context:(id<NSObject>)context {
-    return [CKLabelComponent
-            newWithLabelAttributes:{
-                .string = @"Test"
+    return [CKStackLayoutComponent
+            newWithView:{
+                [UIView class],
+                {{@selector(setBackgroundColor:), [UIColor whiteColor]}}
             }
-            viewAttributes:{}
-            size:{}];
+            size:{}
+            style:{
+                .direction = CKStackLayoutDirectionHorizontal,
+                .alignItems = CKStackLayoutAlignItemsStretch
+            }
+            children:{
+                {
+                    [CKButtonComponent
+                     newWithTitles:{
+                         {UIControlStateNormal, @"Home"}
+                     }
+                     titleColors:{
+                         {UIControlStateNormal, [UIColor colorWithRed:0.259 green:0.259 blue:0.259 alpha:1.00]}
+                     }
+                     images:{}
+                     backgroundImages:{}
+                     titleFont:[UIFont largeTheme]
+                     selected:{}
+                     enabled:YES
+                     action:{}
+                     size:{}
+                     attributes:{}
+                     accessibilityConfiguration:{}],
+                    .flexBasis = CKRelativeDimension::Percent(0.2),
+                    .flexShrink = YES
+                },
+                {
+                    [self verticalSeparator]
+                },
+                {
+                    [CKButtonComponent
+                     newWithTitles:{
+                         {UIControlStateNormal, @"Produk"}
+                     }
+                     titleColors:{
+                         {UIControlStateNormal, [UIColor colorWithRed:0.259 green:0.259 blue:0.259 alpha:1.00]}
+                     }
+                     images:{}
+                     backgroundImages:{}
+                     titleFont:[UIFont largeTheme]
+                     selected:{}
+                     enabled:YES
+                     action:{}
+                     size:{}
+                     attributes:{}
+                     accessibilityConfiguration:{}],
+                    .flexBasis = CKRelativeDimension::Percent(0.2),
+                    .flexShrink = YES
+                },
+                {
+                    [self verticalSeparator]
+                },
+                {
+                    [CKButtonComponent
+                     newWithTitles:{
+                         {UIControlStateNormal, @"Diskusi"}
+                     }
+                     titleColors:{
+                         {UIControlStateNormal, [UIColor colorWithRed:0.259 green:0.259 blue:0.259 alpha:1.00]}
+                     }
+                     images:{}
+                     backgroundImages:{}
+                     titleFont:[UIFont largeTheme]
+                     selected:{}
+                     enabled:YES
+                     action:{}
+                     size:{}
+                     attributes:{}
+                     accessibilityConfiguration:{}],
+                    .flexBasis = CKRelativeDimension::Percent(0.2),
+                    .flexShrink = YES
+                },
+                {
+                    [self verticalSeparator]
+                },
+                {
+                    [CKButtonComponent
+                     newWithTitles:{
+                         {UIControlStateNormal, @"Ulasan"}
+                     }
+                     titleColors:{
+                         {UIControlStateNormal, [UIColor colorWithRed:0.259 green:0.259 blue:0.259 alpha:1.00]}
+                     }
+                     images:{}
+                     backgroundImages:{}
+                     titleFont:[UIFont largeTheme]
+                     selected:{}
+                     enabled:YES
+                     action:{}
+                     size:{}
+                     attributes:{}
+                     accessibilityConfiguration:{}],
+                    .flexBasis = CKRelativeDimension::Percent(0.2),
+                    .flexShrink = YES
+                },
+                {
+                    [self verticalSeparator]
+                },
+                {
+                    [CKButtonComponent
+                     newWithTitles:{
+                         {UIControlStateNormal, @"Catatan"}
+                     }
+                     titleColors:{
+                         {UIControlStateNormal, [UIColor colorWithRed:0.259 green:0.259 blue:0.259 alpha:1.00]}
+                     }
+                     images:{}
+                     backgroundImages:{}
+                     titleFont:[UIFont largeTheme]
+                     selected:{}
+                     enabled:YES
+                     action:{}
+                     size:{}
+                     attributes:{}
+                     accessibilityConfiguration:{}],
+                    .flexBasis = CKRelativeDimension::Percent(0.2),
+                    .flexShrink = YES
+                },
+            }];
 }
 
 @end
