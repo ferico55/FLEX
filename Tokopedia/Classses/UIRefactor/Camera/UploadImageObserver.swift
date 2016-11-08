976307952f27bca31ebb62df9b0ad34d47447355
Renny Runiawati <renny.runiawati@tokopedia.com>
2016-11-08 14:49:50 +0700

    refactor request upload image

--- Tokopedia/Classses/UIRefactor/Camera/UploadImageObserver.swift
+++ Tokopedia/Classses/UIRefactor/Camera/UploadImageObserver.swift
@@ -20,39 +20,41 @@ class UploadImageObserver: NSObject {
         return postData.selectedImages
             .toObservable()
             .flatMap({ (imageObject) -> Observable<ImageResult> in
-                
-                return Observable.create({ (observer) -> Disposable in
-                    
-                    let auth : UserAuthentificationManager = UserAuthentificationManager()
-                    
-                    let postObject :RequestObjectUploadImage = RequestObjectUploadImage()
-                    postObject.user_id = auth.getUserId()
-                    postObject.server_id = postData.host.server_id
-                    postObject.image_id = imageObject.imageID
-                    postObject.web_service = "1"
-                    
-                    let baseURLString = "https://\(postData.host.upload_host)"
-                    
-                    RequestUploadImage.requestUploadImage(imageObject.asset.resizedImage,
-                                                          withUploadHost: baseURLString,
-                        path: "/upload/attachment",
-                        name: "fileToUpload",
-                        fileName: "image.png",
-                        requestObject: postObject,
-                        onSuccess: { (imageResult) in
-                            imageObject.picObj = imageResult.pic_obj
-                            observer.onNext(imageResult)
-                            observer.onCompleted()
-                        }, onFailure: { (error) in
-                            observer.onError(RequestError.networkError)
-                    })
-                    
-                    return NopDisposable.instance
-                })
-                
+                self.requestUploadImage(postData, imageObject: imageObject)
             })
             .toArray()
-        
     }
 
+    class func requestUploadImage(postData: UploadImageRequestObject, imageObject: AttachedImageObject) -> Observable<ImageResult> {
+        
+        return Observable.create({ (observer) -> Disposable in
+            
+            let auth : UserAuthentificationManager = UserAuthentificationManager()
+            
+            let postObject :RequestObjectUploadImage = RequestObjectUploadImage()
+            postObject.user_id = auth.getUserId()
+            postObject.server_id = postData.host.server_id
+            postObject.image_id = imageObject.imageID
+            postObject.web_service = "1"
+            
+            let baseURLString = "https://\(postData.host.upload_host)"
+            
+            RequestUploadImage.requestUploadImage(imageObject.asset.resizedImage,
+                withUploadHost: baseURLString,
+                path: "/upload/attachment",
+                name: "fileToUpload",
+                fileName: "image.png",
+                requestObject: postObject,
+                onSuccess: { (imageResult) in
+                    imageObject.picObj = imageResult.pic_obj
+                    observer.onNext(imageResult)
+                    observer.onCompleted()
+                }, onFailure: { (error) in
+                    observer.onError(RequestError.networkError)
+            })
+            
+            return NopDisposable.instance
+        })
+    }
+    
 }
