dfc17283c24391d64a146bec2f145545f3c1a414
atnlie <atnlie@gmail.com>
2016-11-18 11:01:26 +0700

    IBR-Atnlie

--- Tokopedia/Classses/UIRefactor/Talk/ProductTalkDetailViewController.m
+++ Tokopedia/Classses/UIRefactor/Talk/ProductTalkDetailViewController.m
@@ -269,7 +269,7 @@
 
 #pragma mark - Memory Management
 - (void)dealloc{
-    //NSLog(@"%@ : %@",[self class], NSStringFromSelector(_cmd));
+    NSLog(@"%@ : %@",[self class], NSStringFromSelector(_cmd));
 }
 
 - (void)didReceiveMemoryWarning
@@ -327,7 +327,7 @@
 
     NSInteger row = [self tableView:tableView numberOfRowsInSection:indexPath.section] -1;
     if (row == indexPath.row) {
-        //NSLog(@"%@", NSStringFromSelector(_cmd));
+        NSLog(@"%@", NSStringFromSelector(_cmd));
         
         if (_urinext != NULL && ![_urinext isEqualToString:@"0"] && _urinext != 0) {
             /** called if need to load next page **/
