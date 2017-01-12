3cfad40e30233b9494149cfd7d1dc287c4e55e4c
billionssg <billionssg@yahoo.co.id>
2017-01-12 16:55:54 +0700

    Add user id at slider request (#1615)
    * fixing crash bug Wrong redirect when user fresh install and login to app
    
    * add comment why should use [self view]
    
    * adding Tkpd-UserId when requesting home banner

--- Tokopedia/Classses/TKPStoreManager/TKPHomeBannerStore.m
+++ Tokopedia/Classses/TKPStoreManager/TKPHomeBannerStore.m
@@ -11,6 +11,7 @@
 #import "TKPStoreManager.h"
 #import "MiniSlideObject.h"
 #import "MiniSlide.h"
+#import "UserAuthentificationManager.h"
 
 NSString static *const TKPAPIPageKey = @"page";
 NSString static *const TKPAPILimitKey = @"per_page";
@@ -31,8 +32,12 @@ NSInteger static const TKPSuccessStatusCode = 200;
 - (void)fetchBannerWithCompletion:(void (^)(NSArray<Slide*>*, NSError *))completion {
     RKObjectManager *objectManager = [RKObjectManager sharedClient:@"https://mojito.tokopedia.com/api/v1"];
 
+    UserAuthentificationManager *authManager = [UserAuthentificationManager new];
+
     RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:[SliderObject mapping] method:RKRequestMethodGET pathPattern:@"slides" keyPath:@"" statusCodes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(200, 299)]];
     [objectManager addResponseDescriptor:responseDescriptor];
+    
+    [objectManager.HTTPClient setDefaultHeader:@"Tkpd-UserId" value: [authManager getUserId]];
 
     NSDictionary *parameters = @{@"page[size]" : @"25", @"filter[device]" : @"16", @"filter[target]" : @"65535", @"filter[state]" : @"1"};
     RKObjectRequestOperation *operation = [objectManager appropriateObjectRequestOperationWithObject:nil method:RKRequestMethodGET path:@"slides" parameters:[parameters autoParameters]];
