b4756fef41d95288cd2ddaa19a01d3d937f6a104
esam091 <esam091@gmail.com>
2017-12-06 16:02:21 +0700

    Feature/remove unused objc codes (#2443)
    * remove all messaging codes
    
    * remove JSQMessagesViewController from pods
    
    * remove DetailProductViewController
    
    * remove unused codes
    
    * remove messaging app link
    
    * redirect old message to topchat
    
    * redirect old message to topchat
    
    * remove old navigation code to pdp
    
    * remove unused objc codes using fui
    
    * remove resources txt

--- ios/Tokopedia/Helper/Constants/TKPAPIKey.h
+++
@@ -1,9 +0,0 @@
-//
-//  TKPAPIKey.h
-//  Tokopedia
-//
-//  Created by Renny Runiawati on 10/29/15.
-//  Copyright © 2015 TOKOPEDIA. All rights reserved.
-//
-
-static NSString *const kAPIKey = @"AIzaSyBxw-YVxwb9BQ491BikmOO02TOnPIOuYYU";
