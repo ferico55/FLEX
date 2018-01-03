0ff3bb5266c6bf35aee48e76da74b0b81ed5e0d6
esam091 <esam091@gmail.com>
2018-01-03 16:14:08 +0700

    Improve danger (#2517)
    * Add more files to checklist
    
    * fix script

--- Dangerfile.js
+++ Dangerfile.js
@@ -12,22 +12,24 @@ if (danger.github.pr.changed_files > bigPRThreshold) {
   markdown('> (' + errorCount + ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
 }
 
+const importantFilePatterns = [
+	'AppDelegate.m',
+	'UserAuthentificationManager.m',
+	'MainViewController.m',
+	'Podfile',
+	'package.json',
+	'NetworkProvider.swift',
+	'TKPDHmac.m',
+	'TokopediaNetworkManager.m',
+	'TKPDSecureStorage.m',
+];
 
-const appDelegateFiles = danger.git.modified_files.filter(path => path.startsWith('ios/Tokopedia/AppDelegate')).length > 0
-const userAuthFiles = danger.git.modified_files.filter(path => path.startsWith('ios/Tokopedia/UserAuthentificationManager')).length > 0
-const mainVCFiles = danger.git.modified_files.filter(path => path.startsWith('ios/Tokopedia/MainViewController')).length > 0
-
-if(appDelegateFiles) {
-  	warn(":exclamation: Changes were made in AppDelegate.[h/m]. Please check thoroughly!")
-}
-
-if(userAuthFiles) {
-  	warn(":exclamation: Changes were made in UserAuthenticationManager.[h/m]. Please check thoroughly!")
-}
-
-if(mainVCFiles) {
-	warn(":exclamation: Changes were made in MainViewController.[h/m]. Please check thoroughly!")
-}
+danger.git.modified_files.forEach(path => {
+	const pathIsImportant = importantFilePatterns.some(pattern => path.endsWith(pattern))
+	if (pathIsImportant) {
+		warn(`:exclamation: Changes were made to ${path}, code reviewers check thoroughly`)
+	}
+})
 
 const labels = danger.github.issue.labels.map(label => label.name)
 const _ = require('lodash')
